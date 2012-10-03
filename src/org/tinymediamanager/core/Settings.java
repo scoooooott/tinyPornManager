033296c78b5db0b81afec5b6ddb9a5d19d844d8c
Manuel Laggner <manuel.laggner@gmail.com>
2012-10-03 20:09:58 +0000

    performance at image scaling

--- src/org/tinymediamanager/core/Settings.java
+++ src/org/tinymediamanager/core/Settings.java
@@ -22,197 +22,203 @@ import org.jdesktop.observablecollections.ObservableCollections;
 
 @XmlRootElement(name = "tinyMediaManager")
 public class Settings extends AbstractModelObject {
-	private static Settings instance;
+  private static Settings     instance;
 
-	private final static String CONFIG_FILE = "config" + File.separator
-			+ "config.xml";
-	private final static String MOVIE_DATA_SOURCE = "movieDataSource";
-	private final static String PATH = "path";
-	private final static String VIDEO_FILE_TYPE = "videoFileTypes";
-	private final static String FILETYPE = "filetype";
-	private final static String PROXY_HOST = "proxyHost";
-	private final static String PROXY_PORT = "proxyPort";
-	private final static String PROXY_USERNAME = "proxyUsername";
-	private final static String PROXY_PASSWORD = "proxyPassword";
-	private final static String IMAGE_TMDB_LANGU = "imageTmdbLanguage";
+  private final static String CONFIG_FILE       = "config" + File.separator + "config.xml";
+  private final static String MOVIE_DATA_SOURCE = "movieDataSource";
+  private final static String PATH              = "path";
+  private final static String VIDEO_FILE_TYPE   = "videoFileTypes";
+  private final static String FILETYPE          = "filetype";
+  private final static String PROXY_HOST        = "proxyHost";
+  private final static String PROXY_PORT        = "proxyPort";
+  private final static String PROXY_USERNAME    = "proxyUsername";
+  private final static String PROXY_PASSWORD    = "proxyPassword";
+  private final static String IMAGE_TMDB_LANGU  = "imageTmdbLanguage";
 
-	@XmlElementWrapper(name = VIDEO_FILE_TYPE)
-	@XmlElement(name = FILETYPE)
-	private final List<String> videoFileTypes = new ArrayList<String>();
+  @XmlElementWrapper(name = VIDEO_FILE_TYPE)
+  @XmlElement(name = FILETYPE)
+  private final List<String>  videoFileTypes    = new ArrayList<String>();
 
-	@XmlElementWrapper(name = MOVIE_DATA_SOURCE)
-	@XmlElement(name = PATH)
-	private final List<String> movieDataSources = ObservableCollections
-			.observableList(new ArrayList<String>());
+  @XmlElementWrapper(name = MOVIE_DATA_SOURCE)
+  @XmlElement(name = PATH)
+  private final List<String>  movieDataSources  = ObservableCollections.observableList(new ArrayList<String>());
 
-	private String proxyHost;
-	private String proxyPort;
-	private String proxyUsername;
-	private String proxyPassword;
+  private String              proxyHost;
+  private String              proxyPort;
+  private String              proxyUsername;
+  private String              proxyPassword;
 
-	private String imageTmdbLangugage;
+  private String              imageTmdbLangugage;
 
-	private Settings() {
-	}
+  private Settings() {
+  }
 
-	public static Settings getInstance() {
-		if (Settings.instance == null) {
-			// try to parse XML
-			JAXBContext context;
-			try {
-				context = JAXBContext.newInstance(Settings.class);
-				Unmarshaller um = context.createUnmarshaller();
-				try {
-					Settings.instance = (Settings) um.unmarshal(new FileReader(
-							CONFIG_FILE));
-				} catch (FileNotFoundException e) {
-					// e.printStackTrace();
-					Settings.instance = new Settings();
-					Settings.instance.writeDefaultSettings();
-				} catch (IOException e) {
-					// e.printStackTrace();
-					Settings.instance = new Settings();
-					Settings.instance.writeDefaultSettings();
-				}
-			} catch (JAXBException e) {
-				e.printStackTrace();
-			}
+  public static Settings getInstance() {
+    if (Settings.instance == null) {
+      // try to parse XML
+      JAXBContext context;
+      try {
+        context = JAXBContext.newInstance(Settings.class);
+        Unmarshaller um = context.createUnmarshaller();
+        try {
+          Settings.instance = (Settings) um.unmarshal(new FileReader(CONFIG_FILE));
+        }
+        catch (FileNotFoundException e) {
+          // e.printStackTrace();
+          Settings.instance = new Settings();
+          Settings.instance.writeDefaultSettings();
+        }
+        catch (IOException e) {
+          // e.printStackTrace();
+          Settings.instance = new Settings();
+          Settings.instance.writeDefaultSettings();
+        }
+      }
+      catch (JAXBException e) {
+        e.printStackTrace();
+      }
 
-		}
-		return Settings.instance;
-	}
+    }
+    return Settings.instance;
+  }
 
-	public void addMovieDataSources(String path) {
-		movieDataSources.add(path);
-		firePropertyChange(MOVIE_DATA_SOURCE, null, movieDataSources);
+  public void addMovieDataSources(String path) {
+    movieDataSources.add(path);
+    firePropertyChange(MOVIE_DATA_SOURCE, null, movieDataSources);
 
-	}
+  }
 
-	public void removeMovieDataSources(String path) {
-		movieDataSources.remove(path);
-		firePropertyChange(MOVIE_DATA_SOURCE, null, movieDataSources);
-	}
+  public void removeMovieDataSources(String path) {
+    movieDataSources.remove(path);
+    firePropertyChange(MOVIE_DATA_SOURCE, null, movieDataSources);
+  }
 
-	public List<String> getMovieDataSource() {
-		return movieDataSources;
-	}
+  public List<String> getMovieDataSource() {
+    return movieDataSources;
+  }
 
-	public void addVideoFileTypes(String type) {
-		videoFileTypes.add(type);
-		firePropertyChange(VIDEO_FILE_TYPE, null, videoFileTypes);
-	}
+  public void addVideoFileTypes(String type) {
+    videoFileTypes.add(type);
+    firePropertyChange(VIDEO_FILE_TYPE, null, videoFileTypes);
+  }
 
-	public void removeVideoFileType(String type) {
-		videoFileTypes.remove(type);
-		firePropertyChange(VIDEO_FILE_TYPE, null, videoFileTypes);
-	}
+  public void removeVideoFileType(String type) {
+    videoFileTypes.remove(type);
+    firePropertyChange(VIDEO_FILE_TYPE, null, videoFileTypes);
+  }
 
-	public List<String> getVideoFileType() {
-		return videoFileTypes;
-	}
+  public List<String> getVideoFileType() {
+    return videoFileTypes;
+  }
 
-	public void saveSettings() {
-		// create JAXB context and instantiate marshaller
-		JAXBContext context;
-		Writer w = null;
-		try {
-			context = JAXBContext.newInstance(Settings.class);
-			Marshaller m = context.createMarshaller();
-			m.setProperty("jaxb.encoding", "Unicode");
-			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-			w = new FileWriter(CONFIG_FILE);
-			m.marshal(this, w);
+  public void saveSettings() {
+    // create JAXB context and instantiate marshaller
+    JAXBContext context;
+    Writer w = null;
+    try {
+      context = JAXBContext.newInstance(Settings.class);
+      Marshaller m = context.createMarshaller();
+      m.setProperty("jaxb.encoding", "Unicode");
+      m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+      w = new FileWriter(CONFIG_FILE);
+      m.marshal(this, w);
 
-		} catch (JAXBException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			try {
-				w.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+    }
+    catch (JAXBException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    finally {
+      try {
+        w.close();
+      }
+      catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
 
-		// set proxy information
-		setProxy();
-	}
+    // set proxy information
+    setProxy();
+  }
 
-	private void writeDefaultSettings() {
-		// default video file types
-		addVideoFileTypes(".mpg");
-		addVideoFileTypes(".avi");
-		addVideoFileTypes(".mp4");
-		addVideoFileTypes(".mkv");
+  private void writeDefaultSettings() {
+    // default video file types
+    addVideoFileTypes(".mpg");
+    addVideoFileTypes(".avi");
+    addVideoFileTypes(".mp4");
+    addVideoFileTypes(".mkv");
 
-		setImageTmdbLangugage("de");
+    setImageTmdbLangugage("de");
 
-		saveSettings();
-	}
+    saveSettings();
+  }
 
-	@XmlElement(name = PROXY_HOST)
-	public String getProxyHost() {
-		return proxyHost;
-	}
+  @XmlElement(name = PROXY_HOST)
+  public String getProxyHost() {
+    return proxyHost;
+  }
 
-	public void setProxyHost(String newValue) {
-		String oldValue = this.proxyHost;
-		this.proxyHost = newValue;
-		firePropertyChange(PROXY_HOST, oldValue, newValue);
-	}
+  public void setProxyHost(String newValue) {
+    String oldValue = this.proxyHost;
+    this.proxyHost = newValue;
+    firePropertyChange(PROXY_HOST, oldValue, newValue);
+  }
 
-	@XmlElement(name = PROXY_PORT)
-	public String getProxyPort() {
-		return proxyPort;
-	}
+  @XmlElement(name = PROXY_PORT)
+  public String getProxyPort() {
+    return proxyPort;
+  }
 
-	public void setProxyPort(String newValue) {
-		String oldValue = this.proxyPort;
-		this.proxyPort = newValue;
-		firePropertyChange(PROXY_PORT, oldValue, newValue);
-	}
+  public void setProxyPort(String newValue) {
+    String oldValue = this.proxyPort;
+    this.proxyPort = newValue;
+    firePropertyChange(PROXY_PORT, oldValue, newValue);
+  }
 
-	@XmlElement(name = PROXY_USERNAME)
-	public String getProxyUsername() {
-		return proxyUsername;
-	}
+  @XmlElement(name = PROXY_USERNAME)
+  public String getProxyUsername() {
+    return proxyUsername;
+  }
 
-	public void setProxyUsername(String newValue) {
-		String oldValue = this.proxyUsername;
-		this.proxyUsername = newValue;
-		firePropertyChange(PROXY_USERNAME, oldValue, newValue);
-	}
+  public void setProxyUsername(String newValue) {
+    String oldValue = this.proxyUsername;
+    this.proxyUsername = newValue;
+    firePropertyChange(PROXY_USERNAME, oldValue, newValue);
+  }
 
-	@XmlElement(name = PROXY_PASSWORD)
-	public String getProxyPassword() {
-		return StringEscapeUtils.unescapeXml(proxyPassword);
-	}
+  @XmlElement(name = PROXY_PASSWORD)
+  public String getProxyPassword() {
+    return StringEscapeUtils.unescapeXml(proxyPassword);
+  }
 
-	public void setProxyPassword(String newValue) {
-		newValue = StringEscapeUtils.escapeXml(newValue);
-		String oldValue = this.proxyPassword;
-		this.proxyPassword = newValue;
-		firePropertyChange(PROXY_PASSWORD, oldValue, newValue);
-	}
+  public void setProxyPassword(String newValue) {
+    newValue = StringEscapeUtils.escapeXml(newValue);
+    String oldValue = this.proxyPassword;
+    this.proxyPassword = newValue;
+    firePropertyChange(PROXY_PASSWORD, oldValue, newValue);
+  }
 
-	@XmlElement(name = IMAGE_TMDB_LANGU)
-	public String getImageTmdbLangugage() {
-		return imageTmdbLangugage;
-	}
+  @XmlElement(name = IMAGE_TMDB_LANGU)
+  public String getImageTmdbLangugage() {
+    return imageTmdbLangugage;
+  }
 
-	public void setImageTmdbLangugage(String newValue) {
-		String oldValue = this.imageTmdbLangugage;
-		this.imageTmdbLangugage = newValue;
-		firePropertyChange(IMAGE_TMDB_LANGU, oldValue, newValue);
-	}
+  public void setImageTmdbLangugage(String newValue) {
+    String oldValue = this.imageTmdbLangugage;
+    this.imageTmdbLangugage = newValue;
+    firePropertyChange(IMAGE_TMDB_LANGU, oldValue, newValue);
+  }
 
-	public void setProxy() {
-		System.setProperty("proxyPort", getProxyPort());
-		System.setProperty("proxyHost", getProxyHost());
-		System.setProperty("http.proxyUser", getProxyUsername());
-		System.setProperty("http.proxyPassword", getProxyPassword());
-	}
+  public void setProxy() {
+    if (getProxyHost() != null) {
+      System.setProperty("proxyPort", getProxyPort());
+      System.setProperty("proxyHost", getProxyHost());
+      System.setProperty("http.proxyUser", getProxyUsername());
+      System.setProperty("http.proxyPassword", getProxyPassword());
+    }
+  }
 }
--- src/org/tinymediamanager/ui/ImageLabel.java
+++ src/org/tinymediamanager/ui/ImageLabel.java
@@ -1,126 +1,109 @@
 package org.tinymediamanager.ui;
 
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.JLabel;
 
 import org.tinymediamanager.scraper.util.CachedUrl;
 
+import com.bric.image.pixel.Scaling;
+
 public class ImageLabel extends JLabel {
 
-	private BufferedImage originalImage;
-	private String imageUrl;
-	private String imagePath;
+  private BufferedImage originalImage;
+  private String        imageUrl;
+  private String        imagePath;
 
-	public ImageLabel() {
-		super("");
-	}
+  public ImageLabel() {
+    super("");
+  }
 
-	public String getImagePath() {
-		return imagePath;
-	}
+  public String getImagePath() {
+    return imagePath;
+  }
 
-	public void setImagePath(String newValue) {
-		this.imagePath = newValue;
+  public void setImagePath(String newValue) {
+    this.imagePath = newValue;
 
-		if (newValue == null) {
-			originalImage = null;
-			this.repaint();
-			return;
-		}
+    if (newValue == null) {
+      originalImage = null;
+      this.repaint();
+      return;
+    }
 
-		try {
-			File file = new File(imagePath);
-			if (file.exists()) {
-				this.originalImage = ImageIO.read(file);
-			} else {
-				originalImage = null;
-			}
+    File file = new File(imagePath);
+    if (file.exists()) {
+      this.originalImage = com.bric.image.ImageLoader.createImage(file);// ImageIO.read(file);
+    }
+    else {
+      originalImage = null;
+    }
 
-			this.repaint();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    this.repaint();
+  }
 
-	public String getImageUrl() {
-		return imageUrl;
-	}
+  public String getImageUrl() {
+    return imageUrl;
+  }
 
-	public void setImageUrl(String newValue) {
-		this.imageUrl = newValue;
+  public void setImageUrl(String newValue) {
+    this.imageUrl = newValue;
 
-		if (newValue == null) {
-			originalImage = null;
-			this.repaint();
-			return;
-		}
+    if (newValue == null) {
+      originalImage = null;
+      this.repaint();
+      return;
+    }
 
-		try {
-			CachedUrl cachedUrl = new CachedUrl(imageUrl);
-			this.originalImage = ImageIO.read(cachedUrl.getInputStream(null,
-					true));
+    try {
+      CachedUrl cachedUrl = new CachedUrl(imageUrl);
+      this.originalImage = com.bric.image.ImageLoader.createImage(cachedUrl.getUrl());// ImageIO.read(cachedUrl.getInputStream(null,
+                                                                                      // true));
 
-			this.repaint();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+      this.repaint();
+    }
+    catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
 
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		if (originalImage != null) {
-			int originalWidth = originalImage.getWidth(null);
-			int originalHeight = originalImage.getHeight(null);
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    if (originalImage != null) {
+      int originalWidth = originalImage.getWidth(null);
+      int originalHeight = originalImage.getHeight(null);
 
-			// calculate new height/width
-			Point size = calculateSize(this.getWidth(), this.getHeight(),
-					originalWidth, originalHeight, true);
-			int newWidth = size.x;
-			int newHeight = size.y;
+      // calculate new height/width
+      Point size = calculateSize(this.getWidth(), this.getHeight(), originalWidth, originalHeight, true);
+      int newWidth = size.x;
+      int newHeight = size.y;
+      g.drawImage(Scaling.scale(originalImage, newWidth, newHeight), 0, 0, newWidth, newHeight, this);
+    }
+  }
 
-			// int newWidth, newHeight;
-			// // calculate on available height
-			// newHeight = this.getHeight();
-			// newWidth = newHeight * originalWidth / originalHeight;
-			//
-			// if (newWidth > this.getWidth()) {
-			// // calculate on available height
-			// newWidth = this.getWidth();
-			// newHeight = newWidth * originalHeight / originalWidth;
-			// }
+  public static Point calculateSize(int maxWidth, int maxHeight, int originalWidth, int originalHeight, boolean respectFactor) {
+    Point size = new Point();
+    if (respectFactor) {
+      // calculate on available height
+      size.y = maxHeight;
+      size.x = size.y * originalWidth / originalHeight;
 
-			g.drawImage(originalImage.getScaledInstance(newWidth, newHeight,
-					Image.SCALE_SMOOTH), 0, 0, newWidth, newHeight, this);
-		}
-	}
-
-	public static Point calculateSize(int maxWidth, int maxHeight,
-			int originalWidth, int originalHeight, boolean respectFactor) {
-		Point size = new Point();
-		if (respectFactor) {
-			// calculate on available height
-			size.y = maxHeight;
-			size.x = size.y * originalWidth / originalHeight;
-
-			if (size.x > maxWidth) {
-				// calculate on available height
-				size.x = maxWidth;
-				size.y = size.x * originalHeight / originalWidth;
-			}
-		} else {
-			size.x = maxWidth;
-			size.y = maxHeight;
-		}
-		return size;
-	}
+      if (size.x > maxWidth) {
+        // calculate on available height
+        size.x = maxWidth;
+        size.y = size.x * originalHeight / originalWidth;
+      }
+    }
+    else {
+      size.x = maxWidth;
+      size.y = maxHeight;
+    }
+    return size;
+  }
 
 }
