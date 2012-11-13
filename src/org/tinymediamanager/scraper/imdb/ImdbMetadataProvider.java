9c396e81955586e8e4f28ecc040543b1ceae5c5c
Manuel Laggner <manuel.laggner@gmail.com>
2012-11-13 15:13:43 +0000

    work on IMDB Scraper

--- src/org/tinymediamanager/scraper/imdb/ImdbMetadataProvider.java
+++ src/org/tinymediamanager/scraper/imdb/ImdbMetadataProvider.java
@@ -1,7 +1,5 @@
 package org.tinymediamanager.scraper.imdb;
 
-import java.io.InputStream;
-import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
@@ -11,9 +9,12 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.tinymediamanager.scraper.IHasFindByIMDBID;
 import org.tinymediamanager.scraper.IMediaMetadataProvider;
 import org.tinymediamanager.scraper.MediaMetadata;
@@ -29,44 +30,37 @@ public class ImdbMetadataProvider implements IMediaMetadataProvider, IHasFindByI
   private static final Map<String, ImdbSiteDefinition> IMDB_SITES = new HashMap<String, ImdbSiteDefinition>();
 
   static {
-    IMDB_SITES.put("us", new ImdbSiteDefinition("http://www.imdb.com/", "ISO-8859-1", "Director|Directed by", "Cast", "Release Date", "Runtime",
-        "Country", "Company", "Genre", "Quotes", "Plot", "Rated", "Certification", "Original Air Date", "Writer|Writing credits", "Taglines"));
+    IMDB_SITES.put("us", new ImdbSiteDefinition("http://www.imdb.com/", "ISO-8859-1", "Director|Directed by", "Cast", "Release Date", "Runtime", "Country", "Company", "Genre",
+        "Quotes", "Plot", "Rated", "Certification", "Original Air Date", "Writer|Writing credits", "Taglines"));
 
-    IMDB_SITES.put("fr", new ImdbSiteDefinition("http://www.imdb.fr/", "ISO-8859-1", "R&#xE9;alisateur|R&#xE9;alis&#xE9; par", "Ensemble",
-        "Date de sortie", "Dur&#xE9;e", "Pays", "Soci&#xE9;t&#xE9;", "Genre", "Citation", "Intrigue", "Rated", "Classification", "Date de sortie",
-        "Sc&#xE9;naristes|Sc&#xE9;naristes", "Taglines"));
+    IMDB_SITES.put("fr", new ImdbSiteDefinition("http://www.imdb.fr/", "ISO-8859-1", "R&#xE9;alisateur|R&#xE9;alis&#xE9; par", "Ensemble", "Date de sortie", "Dur&#xE9;e", "Pays",
+        "Soci&#xE9;t&#xE9;", "Genre", "Citation", "Intrigue", "Rated", "Classification", "Date de sortie", "Sc&#xE9;naristes|Sc&#xE9;naristes", "Taglines"));
 
-    IMDB_SITES.put("es", new ImdbSiteDefinition("http://www.imdb.es/", "ISO-8859-1", "Director|Dirigida por", "Reparto", "Fecha de Estreno",
-        "Duraci&#xF3;n", "Pa&#xED;s", "Compa&#xF1;&#xED;a", "G&#xE9;nero", "Quotes", "Trama", "Rated", "Clasificaci&#xF3;n", "Fecha de Estreno",
-        "Escritores|Cr&#xE9;ditos del gui&#xF3;n", "Taglines"));
+    IMDB_SITES.put("es", new ImdbSiteDefinition("http://www.imdb.es/", "ISO-8859-1", "Director|Dirigida por", "Reparto", "Fecha de Estreno", "Duraci&#xF3;n", "Pa&#xED;s",
+        "Compa&#xF1;&#xED;a", "G&#xE9;nero", "Quotes", "Trama", "Rated", "Clasificaci&#xF3;n", "Fecha de Estreno", "Escritores|Cr&#xE9;ditos del gui&#xF3;n", "Taglines"));
 
-    IMDB_SITES.put("de", new ImdbSiteDefinition("http://www.imdb.de/", "ISO-8859-1", "Regisseur|Regie", "Besetzung", "Premierendatum", "L&#xE4;nge",
-        "Land", "Firma", "Genre", "Quotes", "Handlung", "Rated", "Altersfreigabe", "Premierendatum", "Guionista|Buch", "Taglines"));
+    IMDB_SITES.put("de", new ImdbSiteDefinition("http://www.imdb.de/", "ISO-8859-1", "Regisseur|Regie", "Besetzung", "Premierendatum", "L&#xE4;nge", "Land", "Firma", "Genre",
+        "Quotes", "Handlung", "Rated", "Altersfreigabe", "Premierendatum", "Guionista|Buch", "Taglines"));
 
-    IMDB_SITES.put("it", new ImdbSiteDefinition("http://www.imdb.it/", "ISO-8859-1", "Regista|Registi|Regia di", "Cast", "Data di uscita", "Durata",
-        "Nazionalit&#xE0;", "Compagnia", "Genere", "Quotes", "Trama", "Rated", "Certification", "Data di uscita", "Sceneggiatore|Scritto da",
-        "Taglines"));
+    IMDB_SITES.put("it", new ImdbSiteDefinition("http://www.imdb.it/", "ISO-8859-1", "Regista|Registi|Regia di", "Cast", "Data di uscita", "Durata", "Nazionalit&#xE0;",
+        "Compagnia", "Genere", "Quotes", "Trama", "Rated", "Certification", "Data di uscita", "Sceneggiatore|Scritto da", "Taglines"));
 
-    IMDB_SITES.put("pt", new ImdbSiteDefinition("http://www.imdb.pt/", "ISO-8859-1", "Diretor|Dirigido por", "Elenco", "Data de Lan&#xE7;amento",
-        "Dura&#xE7;&#xE3;o", "Pa&#xED;s", "Companhia", "G&#xEA;nero", "Quotes", "Argumento", "Rated", "Certifica&#xE7;&#xE3;o",
-        "Data de Lan&#xE7;amento", "Roteirista|Cr&#xE9;ditos como roteirista", "Taglines"));
+    IMDB_SITES.put("pt", new ImdbSiteDefinition("http://www.imdb.pt/", "ISO-8859-1", "Diretor|Dirigido por", "Elenco", "Data de Lan&#xE7;amento", "Dura&#xE7;&#xE3;o", "Pa&#xED;s",
+        "Companhia", "G&#xEA;nero", "Quotes", "Argumento", "Rated", "Certifica&#xE7;&#xE3;o", "Data de Lan&#xE7;amento", "Roteirista|Cr&#xE9;ditos como roteirista", "Taglines"));
 
     // Use this as a workaround for English speakers abroad who get localised
     // versions of imdb.com
-    IMDB_SITES.put("labs", new ImdbSiteDefinition("http://akas.imdb.com/", "ISO-8859-1", "Director|Directors|Directed by", "Cast", "Release Date",
-        "Runtime", "Country", "Production Co", "Genres", "Quotes", "Storyline", "Rated", "Certification", "Original Air Date",
-        "Writer|Writers|Writing credits", "Taglines"));
+    IMDB_SITES.put("labs", new ImdbSiteDefinition("http://akas.imdb.com/", "ISO-8859-1", "Director|Directors|Directed by", "Cast", "Release Date", "Runtime", "Country",
+        "Production Co", "Genres", "Quotes", "Storyline", "Rated", "Certification", "Original Air Date", "Writer|Writers|Writing credits", "Taglines"));
 
     // TODO: Leaving this as labs.imdb.com for the time being, but will be
     // updated to www.imdb.com
-    IMDB_SITES.put("us2", new ImdbSiteDefinition("http://labs.imdb.com/", "ISO-8859-1", "Director|Directors|Directed by", "Cast", "Release Date",
-        "Runtime", "Country", "Production Co", "Genres", "Quotes", "Storyline", "Rated", "Certification", "Original Air Date",
-        "Writer|Writers|Writing credits", "Taglines"));
+    IMDB_SITES.put("us2", new ImdbSiteDefinition("http://labs.imdb.com/", "ISO-8859-1", "Director|Directors|Directed by", "Cast", "Release Date", "Runtime", "Country",
+        "Production Co", "Genres", "Quotes", "Storyline", "Rated", "Certification", "Original Air Date", "Writer|Writers|Writing credits", "Taglines"));
 
     // Not 100% sure these are correct
-    IMDB_SITES.put("it2", new ImdbSiteDefinition("http://www.imdb.it/", "ISO-8859-1", "Regista|Registi|Regia di", "Attori", "Data di uscita",
-        "Durata", "Nazionalit&#xE0;", "Compagnia", "Genere", "Quotes", "Trama", "Rated", "Certification", "Data di uscita",
-        "Sceneggiatore|Scritto da", "Taglines"));
+    IMDB_SITES.put("it2", new ImdbSiteDefinition("http://www.imdb.it/", "ISO-8859-1", "Regista|Registi|Regia di", "Attori", "Data di uscita", "Durata", "Nazionalit&#xE0;",
+        "Compagnia", "Genere", "Quotes", "Trama", "Rated", "Certification", "Data di uscita", "Sceneggiatore|Scritto da", "Taglines"));
   }
 
   private ImdbSiteDefinition                           imdbSite;
@@ -158,8 +152,7 @@ public class ImdbMetadataProvider implements IMediaMetadataProvider, IHasFindByI
     sb.append("find?q=");
     try {
       sb.append(URLEncoder.encode(searchTerm, imdbSite.getCharset().displayName()));
-    }
-    catch (UnsupportedEncodingException ex) {
+    } catch (UnsupportedEncodingException ex) {
       // Failed to encode the movie name for some reason!
       LOGGER.debug("Failed to encode search term: " + searchTerm);
       sb.append(searchTerm);
@@ -172,85 +165,69 @@ public class ImdbMetadataProvider implements IMediaMetadataProvider, IHasFindByI
     sb.append(";s=tt;site=aka");
 
     LOGGER.debug("========= BEGIN IMDB Scraper Search for: " + sb.toString());
-    String xml;
+    Document doc;
     try {
       CachedUrl url = new CachedUrl(sb.toString());
-      InputStream is = url.getInputStream();
-      StringWriter writer = new StringWriter();
-      IOUtils.copy(is, writer);
-      xml = writer.toString();
-      is.close();
-      writer.close();
-    }
-    catch (Exception e) {
+      doc = Jsoup.parse(url.getInputStream(), imdbSite.getCharset().displayName(), "");
+    } catch (Exception e) {
       LOGGER.debug("tried to fetch search response", e);
       return result;
     }
 
-    // Check if this is an exact match (we got a movie page instead of a results
-    // list)
-    Pattern titleregex = Pattern.compile(Pattern.quote("<link rel=\"canonical\" href=\"" + imdbSite.getSite() + "title/" + "(tt\\d+)/\""));
-    Matcher titlematch = titleregex.matcher(xml);
-    if (titlematch.find()) {
-      LOGGER.debug("IMDb returned one match " + titlematch.group(1));
-      // return titlematch.group(1);
-    }
+    // check if it was directly redirected to the site
+    // ToDo
 
-    String otherMovieName = HTMLTools.extractTag(HTMLTools.extractTag(xml, ";ttype=ep\">", "\"</a>.</li>"), "<b>", "</b>").toLowerCase();
-    String formattedMovieName;
-    if (!StringUtils.isEmpty(otherMovieName)) {
-      // if (StringTools.isValidString(year) && otherMovieName.endsWith(")") &&
-      // otherMovieName.contains("(")) {
-      // otherMovieName = otherMovieName.substring(0,
-      // otherMovieName.lastIndexOf('(') - 1);
-      // formattedMovieName = otherMovieName + "</a> (" + year + ")";
-      // }
-      // else {
-      formattedMovieName = otherMovieName + "</a>";
-      // }
-    }
-    else {
-      sb = new StringBuilder();
-      try {
-        sb.append(URLEncoder.encode(searchTerm, imdbSite.getCharset().displayName()).replace("+", " "));
+    // parse results
+    Elements elements = doc.getElementsByAttributeValue("valign", "top");
+    for (Element element : elements) {
+      // we only want the td's
+      if (!"td".equalsIgnoreCase(element.tagName())) {
+        continue;
       }
-      catch (UnsupportedEncodingException ex) {
-        LOGGER.debug("Failed to encode movie name: " + searchTerm);
-        sb.append(searchTerm);
-      }
-      sb.append("</a>");
-      // if (StringTools.isValidString(year)) {
-      // sb.append(" (").append(year).append(")");
-      // }
-      otherMovieName = sb.toString();
-      formattedMovieName = otherMovieName;
-    }
 
-    // logger.debug(logMessage + "Title search: '" + formattedMovieName +
-    // "'"); // XXX DEBUG
-    for (String searchResult : HTMLTools.extractTags(xml, "<div class=\"media_strip_thumbs\">", "<div id=\"sidebar\">", ".src='/rg/find-" + "title-",
-        "</td>", false)) {
-      // logger.debug(logMessage + "Title check  : '" + searchResult + "'");
-      // // XXX DEBUG
-      if (searchResult.toLowerCase().indexOf(formattedMovieName) != -1) {
-        // logger.debug(logMessage + "Title match  : '" + searchResult + "'");
-        // // XXX DEBUG
-        MediaSearchResult sr = new MediaSearchResult();
-        sr.setTitle(HTMLTools.extractTag(searchResult, "/images/b.gif?link=" + "/title/", "/';\">"));
-        result.add(sr);
-      }
-      else {
-        for (String otherResult : HTMLTools.extractTags(searchResult, "</';\">", "</p>", "<p class=\"find-aka\">", "</em>", false)) {
-          if (otherResult.toLowerCase().indexOf("\"" + otherMovieName + "\"") != -1) {
-            // logger.debug(logMessage + "Other Title match: '" + otherResult
-            // + "'"); // XXX DEBUG
+      // get the name inside the link
+      String movieName = null;
+      String movieId = null;
+      Elements anchors = element.getElementsByTag("a");
+      for (Element a : anchors) {
+        if (StringUtils.isEmpty(a.text())) {
+          continue;
+        }
 
-            MediaSearchResult sr = new MediaSearchResult();
-            sr.setTitle(HTMLTools.extractTag(searchResult, "/images/b.gif?link=" + "/title/", "/';\">"));
-            result.add(sr);
-          }
+        // movie name
+        movieName = a.text();
+
+        // parse id
+        String href = a.attr("href");
+        if (!StringUtils.isEmpty(href) && href.contains("/title/tt")) {
+          int length = href.length();
+          movieId = href.substring(7, length - 1);
+        }
+
+        break;
+      }
+
+      // if no movie name/id was found - continue
+      if (StringUtils.isEmpty(movieName) || StringUtils.isEmpty(movieId)) {
+        continue;
+      }
+
+      MediaSearchResult sr = new MediaSearchResult();
+      sr.setTitle(movieName);
+      sr.setIMDBId(movieId);
+
+      // try to parse out the year
+      Pattern yearPattern = Pattern.compile("\\(([0-9]{4})|/\\)");
+      Matcher matcher = yearPattern.matcher(element.text());
+      while (matcher.find()) {
+        if (matcher.group(1) != null) {
+          sr.setYear(matcher.group(1));
+          break;
         }
       }
+
+      result.add(sr);
+
     }
 
     return result;
