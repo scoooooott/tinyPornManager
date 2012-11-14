680ac108ece16ad56dc4b9f355e7c9617a4da3df
Manuel Laggner <manuel.laggner@gmail.com>
2012-11-14 14:07:26 +0000

    added some test cases for imdb scraper

--- src/org/tinymediamanager/scraper/imdb/ImdbMetadataProvider.java
+++ src/org/tinymediamanager/scraper/imdb/ImdbMetadataProvider.java
@@ -3,9 +3,7 @@ package org.tinymediamanager.scraper.imdb;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -28,55 +26,83 @@ import org.tinymediamanager.scraper.SearchQuery;
 import org.tinymediamanager.scraper.util.CachedUrl;
 
 public class ImdbMetadataProvider implements IMediaMetadataProvider, IHasFindByIMDBID {
-  private static final Logger                          LOGGER     = Logger.getLogger(ImdbMetadataProvider.class);
+  private static final Logger LOGGER = Logger.getLogger(ImdbMetadataProvider.class);
 
-  private static final Map<String, ImdbSiteDefinition> IMDB_SITES = new HashMap<String, ImdbSiteDefinition>();
+  // private static final Map<String, ImdbSiteDefinition> IMDB_SITES = new
+  // HashMap<String, ImdbSiteDefinition>();
 
-  static {
-    IMDB_SITES.put("us", new ImdbSiteDefinition("http://www.imdb.com/", "ISO-8859-1", "Director|Directed by", "Cast", "Release Date", "Runtime",
-        "Country", "Company", "Genre", "Quotes", "Plot", "Rated", "Certification", "Original Air Date", "Writer|Writing credits", "Taglines"));
+  // static {
+  // IMDB_SITES.put("us", new ImdbSiteDefinition("http://www.imdb.com/",
+  // "ISO-8859-1", "Director|Directed by", "Cast", "Release Date", "Runtime",
+  // "Country", "Company", "Genre",
+  // "Quotes", "Plot", "Rated", "Certification", "Original Air Date",
+  // "Writer|Writing credits", "Taglines"));
+  //
+  // IMDB_SITES.put("fr", new ImdbSiteDefinition("http://www.imdb.fr/",
+  // "ISO-8859-1", "R&#xE9;alisateur|R&#xE9;alis&#xE9; par", "Ensemble",
+  // "Date de sortie", "Dur&#xE9;e", "Pays",
+  // "Soci&#xE9;t&#xE9;", "Genre", "Citation", "Intrigue", "Rated",
+  // "Classification", "Date de sortie", "Sc&#xE9;naristes|Sc&#xE9;naristes",
+  // "Taglines"));
+  //
+  // IMDB_SITES.put("es", new ImdbSiteDefinition("http://www.imdb.es/",
+  // "ISO-8859-1", "Director|Dirigida por", "Reparto", "Fecha de Estreno",
+  // "Duraci&#xF3;n", "Pa&#xED;s",
+  // "Compa&#xF1;&#xED;a", "G&#xE9;nero", "Quotes", "Trama", "Rated",
+  // "Clasificaci&#xF3;n", "Fecha de Estreno",
+  // "Escritores|Cr&#xE9;ditos del gui&#xF3;n", "Taglines"));
+  //
+  // IMDB_SITES.put("de", new ImdbSiteDefinition("http://www.imdb.de/",
+  // "ISO-8859-1", "Regisseur|Regie", "Besetzung", "Premierendatum",
+  // "L&#xE4;nge", "Land", "Firma", "Genre",
+  // "Quotes", "Handlung", "Rated", "Altersfreigabe", "Premierendatum",
+  // "Guionista|Buch", "Taglines"));
+  //
+  // IMDB_SITES.put("it", new ImdbSiteDefinition("http://www.imdb.it/",
+  // "ISO-8859-1", "Regista|Registi|Regia di", "Cast", "Data di uscita",
+  // "Durata", "Nazionalit&#xE0;",
+  // "Compagnia", "Genere", "Quotes", "Trama", "Rated", "Certification",
+  // "Data di uscita", "Sceneggiatore|Scritto da", "Taglines"));
+  //
+  // IMDB_SITES.put("pt", new ImdbSiteDefinition("http://www.imdb.pt/",
+  // "ISO-8859-1", "Diretor|Dirigido por", "Elenco", "Data de Lan&#xE7;amento",
+  // "Dura&#xE7;&#xE3;o", "Pa&#xED;s",
+  // "Companhia", "G&#xEA;nero", "Quotes", "Argumento", "Rated",
+  // "Certifica&#xE7;&#xE3;o", "Data de Lan&#xE7;amento",
+  // "Roteirista|Cr&#xE9;ditos como roteirista", "Taglines"));
+  //
+  // // Use this as a workaround for English speakers abroad who get localised
+  // // versions of imdb.com
+  // IMDB_SITES.put("labs", new ImdbSiteDefinition("http://akas.imdb.com/",
+  // "ISO-8859-1", "Director|Directors|Directed by", "Cast", "Release Date",
+  // "Runtime", "Country",
+  // "Production Co", "Genres", "Quotes", "Storyline", "Rated", "Certification",
+  // "Original Air Date", "Writer|Writers|Writing credits", "Taglines"));
+  //
+  // // TODO: Leaving this as labs.imdb.com for the time being, but will be
+  // // updated to www.imdb.com
+  // IMDB_SITES.put("us2", new ImdbSiteDefinition("http://labs.imdb.com/",
+  // "ISO-8859-1", "Director|Directors|Directed by", "Cast", "Release Date",
+  // "Runtime", "Country",
+  // "Production Co", "Genres", "Quotes", "Storyline", "Rated", "Certification",
+  // "Original Air Date", "Writer|Writers|Writing credits", "Taglines"));
+  //
+  // // Not 100% sure these are correct
+  // IMDB_SITES.put("it2", new ImdbSiteDefinition("http://www.imdb.it/",
+  // "ISO-8859-1", "Regista|Registi|Regia di", "Attori", "Data di uscita",
+  // "Durata", "Nazionalit&#xE0;",
+  // "Compagnia", "Genere", "Quotes", "Trama", "Rated", "Certification",
+  // "Data di uscita", "Sceneggiatore|Scritto da", "Taglines"));
+  // }
 
-    IMDB_SITES.put("fr", new ImdbSiteDefinition("http://www.imdb.fr/", "ISO-8859-1", "R&#xE9;alisateur|R&#xE9;alis&#xE9; par", "Ensemble",
-        "Date de sortie", "Dur&#xE9;e", "Pays", "Soci&#xE9;t&#xE9;", "Genre", "Citation", "Intrigue", "Rated", "Classification", "Date de sortie",
-        "Sc&#xE9;naristes|Sc&#xE9;naristes", "Taglines"));
-
-    IMDB_SITES.put("es", new ImdbSiteDefinition("http://www.imdb.es/", "ISO-8859-1", "Director|Dirigida por", "Reparto", "Fecha de Estreno",
-        "Duraci&#xF3;n", "Pa&#xED;s", "Compa&#xF1;&#xED;a", "G&#xE9;nero", "Quotes", "Trama", "Rated", "Clasificaci&#xF3;n", "Fecha de Estreno",
-        "Escritores|Cr&#xE9;ditos del gui&#xF3;n", "Taglines"));
-
-    IMDB_SITES.put("de", new ImdbSiteDefinition("http://www.imdb.de/", "ISO-8859-1", "Regisseur|Regie", "Besetzung", "Premierendatum", "L&#xE4;nge",
-        "Land", "Firma", "Genre", "Quotes", "Handlung", "Rated", "Altersfreigabe", "Premierendatum", "Guionista|Buch", "Taglines"));
-
-    IMDB_SITES.put("it", new ImdbSiteDefinition("http://www.imdb.it/", "ISO-8859-1", "Regista|Registi|Regia di", "Cast", "Data di uscita", "Durata",
-        "Nazionalit&#xE0;", "Compagnia", "Genere", "Quotes", "Trama", "Rated", "Certification", "Data di uscita", "Sceneggiatore|Scritto da",
-        "Taglines"));
-
-    IMDB_SITES.put("pt", new ImdbSiteDefinition("http://www.imdb.pt/", "ISO-8859-1", "Diretor|Dirigido por", "Elenco", "Data de Lan&#xE7;amento",
-        "Dura&#xE7;&#xE3;o", "Pa&#xED;s", "Companhia", "G&#xEA;nero", "Quotes", "Argumento", "Rated", "Certifica&#xE7;&#xE3;o",
-        "Data de Lan&#xE7;amento", "Roteirista|Cr&#xE9;ditos como roteirista", "Taglines"));
-
-    // Use this as a workaround for English speakers abroad who get localised
-    // versions of imdb.com
-    IMDB_SITES.put("labs", new ImdbSiteDefinition("http://akas.imdb.com/", "ISO-8859-1", "Director|Directors|Directed by", "Cast", "Release Date",
-        "Runtime", "Country", "Production Co", "Genres", "Quotes", "Storyline", "Rated", "Certification", "Original Air Date",
-        "Writer|Writers|Writing credits", "Taglines"));
-
-    // TODO: Leaving this as labs.imdb.com for the time being, but will be
-    // updated to www.imdb.com
-    IMDB_SITES.put("us2", new ImdbSiteDefinition("http://labs.imdb.com/", "ISO-8859-1", "Director|Directors|Directed by", "Cast", "Release Date",
-        "Runtime", "Country", "Production Co", "Genres", "Quotes", "Storyline", "Rated", "Certification", "Original Air Date",
-        "Writer|Writers|Writing credits", "Taglines"));
-
-    // Not 100% sure these are correct
-    IMDB_SITES.put("it2", new ImdbSiteDefinition("http://www.imdb.it/", "ISO-8859-1", "Regista|Registi|Regia di", "Attori", "Data di uscita",
-        "Durata", "Nazionalit&#xE0;", "Compagnia", "Genere", "Quotes", "Trama", "Rated", "Certification", "Data di uscita",
-        "Sceneggiatore|Scritto da", "Taglines"));
-  }
-
-  private ImdbSiteDefinition                           imdbSite;
+  private ImdbSiteDefinition  imdbSite;
 
   public ImdbMetadataProvider() {
-    imdbSite = IMDB_SITES.get("de");
+    imdbSite = ImdbSiteDefinition.IMDB_COM;
+  }
+
+  public ImdbMetadataProvider(ImdbSiteDefinition site) {
+    imdbSite = site;
   }
 
   /*
@@ -102,8 +128,7 @@ public class ImdbMetadataProvider implements IMediaMetadataProvider, IHasFindByI
     try {
       CachedUrl url = new CachedUrl(sb.toString());
       doc = Jsoup.parse(url.getInputStream(), imdbSite.getCharset().displayName(), "");
-    }
-    catch (Exception e) {
+    } catch (Exception e) {
       LOGGER.debug("tried to fetch imdb movie page", e);
       return md;
     }
@@ -221,8 +246,7 @@ public class ImdbMetadataProvider implements IMediaMetadataProvider, IHasFindByI
     if (result.getMetadata() != null) {
       LOGGER.debug("IMDB: getMetadata(result) from cache: " + result);
       return result.getMetadata();
-    }
-    else {
+    } else {
       LOGGER.debug("IMDB: getMetadata(result): " + result);
       String imdbId = result.getIMDBId();
       return getMetadataForIMDBId(imdbId);
@@ -265,8 +289,7 @@ public class ImdbMetadataProvider implements IMediaMetadataProvider, IHasFindByI
     sb.append("find?q=");
     try {
       sb.append(URLEncoder.encode(searchTerm, imdbSite.getCharset().displayName()));
-    }
-    catch (UnsupportedEncodingException ex) {
+    } catch (UnsupportedEncodingException ex) {
       // Failed to encode the movie name for some reason!
       LOGGER.debug("Failed to encode search term: " + searchTerm);
       sb.append(searchTerm);
@@ -279,8 +302,7 @@ public class ImdbMetadataProvider implements IMediaMetadataProvider, IHasFindByI
     try {
       CachedUrl url = new CachedUrl(sb.toString());
       doc = Jsoup.parse(url.getInputStream(), imdbSite.getCharset().displayName(), "");
-    }
-    catch (Exception e) {
+    } catch (Exception e) {
       LOGGER.debug("tried to fetch search response", e);
       return result;
     }
---
+++ test/org/tinymediamanager/scraper/imdb/ImdbMetadataProviderTest.java
@@ -0,0 +1,185 @@
+package org.tinymediamanager.scraper.imdb;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.tinymediamanager.scraper.MediaMetadata;
+import org.tinymediamanager.scraper.MediaSearchResult;
+import org.tinymediamanager.scraper.MediaType;
+import org.tinymediamanager.scraper.SearchQuery;
+
+public class ImdbMetadataProviderTest {
+
+  @Test
+  public void testSearch() {
+    ImdbMetadataProvider mp = null;
+    List<MediaSearchResult> results = null;
+
+    /*
+     * test on akas.imdb.com - "9"
+     */
+    results = null;
+    try {
+      mp = new ImdbMetadataProvider(ImdbSiteDefinition.IMDB_COM);
+      results = mp.search(new SearchQuery(MediaType.MOVIE, SearchQuery.Field.QUERY, "9"));
+    } catch (Exception e) {
+    }
+
+    // did we get a result?
+    assertNotNull("Result", results);
+
+    // result count
+    assertEquals("Result count", 5, results.size());
+
+    // check first result (9 - 2009 - tt0472033)
+    MediaSearchResult result = results.get(0);
+    checkSearchResult("9", "2009", "tt0472033", result);
+
+    // check second result (9 - 2005 - tt0443424)
+    result = results.get(1);
+    checkSearchResult("9", "2005", "tt0443424", result);
+
+    // check third result (9 - 2002 - tt0342012)
+    result = results.get(2);
+    checkSearchResult("9", "2002", "tt0342012", result);
+
+    // check fourth result (9 - 2009 - tt1430606)
+    result = results.get(3);
+    checkSearchResult("9", "2009", "tt1430606", result);
+
+    // check fifth result (9 - 1996 - tt0191312)
+    result = results.get(4);
+    checkSearchResult("9", "1996", "tt0191312", result);
+
+    /*
+     * test on akas.imdb.com - "Inglorious Basterds"
+     */
+    results = null;
+    try {
+      mp = new ImdbMetadataProvider(ImdbSiteDefinition.IMDB_COM);
+      results = mp.search(new SearchQuery(MediaType.MOVIE, SearchQuery.Field.QUERY, "Inglorious Basterds"));
+    } catch (Exception e) {
+    }
+
+    // did we get a result?
+    assertNotNull("Result", results);
+
+    // result count
+    assertEquals("Result count", 10, results.size());
+
+    // check first result (Inglourious Basterds - 2009 - tt0361748)
+    result = results.get(0);
+    checkSearchResult("Inglourious Basterds", "2009", "tt0361748", result);
+
+    // check second result (Quel maledetto treno blindato - 1978 - tt0076584)
+    result = results.get(1);
+    checkSearchResult("Quel maledetto treno blindato", "1978", "tt0076584", result);
+
+    // check third result (Eroi dell'inferno - 1987 - tt0342012)
+    result = results.get(2);
+    checkSearchResult("Eroi dell'inferno", "1987", "tt0422182", result);
+
+    // check 4. result (Inglourious Basterds: Done in 60 seconds - 2010 -
+    // tt1430606)
+    result = results.get(3);
+    checkSearchResult("Inglourious Basterds: Done in 60 seconds", "2010", "tt1837578", result);
+
+    // check 5. result (Inglourious Basterds: Movie Special - 2009 -
+    // tt1515156)
+    result = results.get(4);
+    checkSearchResult("Inglourious Basterds: Movie Special", "2009", "tt1515156", result);
+
+    // check 6. result (Quelli del Maledetto Treno Blindato - Making of
+    // 'Inglorious Bastards' - 2007 -
+    // tt1028564)
+    result = results.get(5);
+    checkSearchResult("Quelli del Maledetto Treno Blindato - Making of 'Inglorious Bastards'", "2007", "tt1028564", result);
+
+    // check 7. result (Merah Putih - 2009 - tt1438496)
+    result = results.get(6);
+    checkSearchResult("Merah Putih", "2009", "tt1438496", result);
+
+    // check 8. result (Joshikyôei hanrangun - 2007 - tt1134826)
+    result = results.get(7);
+    checkSearchResult("Joshikyôei hanrangun", "2007", "tt1134826", result);
+
+    // check 9. result (Inglorious Bumblers - 2009 - tt1529278)
+    result = results.get(8);
+    checkSearchResult("Inglorious Bumblers", "2009", "tt1529278", result);
+
+    // check 10. result (Inglorious Relations - 2008 - tt1378268)
+    result = results.get(9);
+    checkSearchResult("Inglorious Relations", "2008", "tt1378268", result);
+  }
+
+  private void checkSearchResult(String title, String year, String imdbId, MediaSearchResult result) {
+    // title
+    assertEquals("title", title, result.getTitle());
+    // year
+    assertEquals("year", year, result.getYear());
+    // imdbId
+    assertEquals("imdbId", imdbId, result.getIMDBId());
+  }
+
+  @Test
+  public void testScrape() {
+    ImdbMetadataProvider mp = null;
+    MediaSearchResult sr = null;
+    MediaMetadata md = null;
+
+    /*
+     * scrape akas.imdb.com - 9 - tt0472033
+     */
+    mp = new ImdbMetadataProvider(ImdbSiteDefinition.IMDB_COM);
+    sr = new MediaSearchResult();
+    sr.setIMDBId("tt0472033");
+    sr.setTitle("9");
+    sr.setYear("2009");
+
+    md = null;
+    try {
+      md = mp.getMetaData(sr);
+    } catch (Exception e) {
+    }
+
+    // did we get metadata?
+    assertNotNull("MediaMetadata", md);
+
+    // check moviedetails
+    checkMovieDetails("9", "2009", "9", md);
+
+    /*
+     * scrape akas.imdb.com - 12 Monkeys - tt0114746
+     */
+    mp = new ImdbMetadataProvider(ImdbSiteDefinition.IMDB_COM);
+    sr = new MediaSearchResult();
+    sr.setIMDBId("tt0114746");
+    sr.setTitle("Twelve Monkeys");
+    sr.setYear("1995");
+
+    md = null;
+    try {
+      md = mp.getMetaData(sr);
+    } catch (Exception e) {
+    }
+
+    // did we get metadata?
+    assertNotNull("MediaMetadata", md);
+
+    // check moviedetails
+    checkMovieDetails("Twelve Monkeys", "1995", "Twelve Monkeys", md);
+
+  }
+
+  private void checkMovieDetails(String title, String year, String originalTitle, MediaMetadata md) {
+    // title
+    assertEquals("title", title, md.getMediaTitle());
+    // year
+    assertEquals("year", year, md.getYear());
+    // original title
+    assertEquals("originalTitle", originalTitle, md.getOriginalTitle());
+  }
+}
