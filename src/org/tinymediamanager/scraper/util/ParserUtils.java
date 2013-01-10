91a01df48eba0b8137284971027be03184f5c374
Myron <myron0815@gmx.net>
2013-01-10 21:03:46 +0000

    Parse moviename from filename

--- src/org/tinymediamanager/scraper/util/ParserUtils.java
+++ src/org/tinymediamanager/scraper/util/ParserUtils.java
@@ -15,16 +15,87 @@
  */
 package org.tinymediamanager.scraper.util;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.apache.log4j.Logger;
 
 /**
  * The Class ParserUtils.
  */
 public class ParserUtils {
 
+  private static final Logger LOGGER         = Logger.getLogger(ParserUtils.class);
+
   /** The date in brackets. */
-  private static Pattern dateInBrackets = Pattern.compile("(.*)\\s+\\(([0-9]{4})\\)");
+  private static Pattern      dateInBrackets = Pattern.compile("(.*)\\s+\\(([0-9]{4})\\)");
+
+  /**
+   * Tries to get movie name from filename<br>
+   * 1. splits string using common delimiters ".- ()"<br>
+   * 2. searches for first occurrence of common stopwords<br>
+   * 3. if last token is 4 digits, assume year and remove (only if we have at
+   * least 3 tokens)<br>
+   * 4. everything before the first stopword must be the movie name :p
+   * 
+   * @param filename
+   *          the filename to get the title from
+   * @return the (hopefully) correct parsed movie name
+   * @author Myron Boyle
+   */
+  public static String detectCleanMoviename(String filename) {
+    LOGGER.debug("Parse filename for movie title: \"" + filename + "\"");
+
+    String[] stopwords = { "ac3", "dts", "custom", "docu", "dc", "dtv", "divx", "divx5", "dsr", "dsrip", "dutch", "dvd", "dvdrip", "dvdscr",
+        "dvdscreener", "screener", "dvdivx", "cam", "fragment", "fs", "german", "hdtv", "hdrip", "hdtvrip", "internal", "limited", "multisubs",
+        "ntsc", "ogg", "ogm", "pal", "pdtv", "proper", "repack", "rerip", "retail", "r3", "r5", "bd5", "se", "svcd", "swedish", "german", "read.nfo",
+        "nfofix", "unrated", "ws", "telesync", "ts", "telecine", "tc", "brrip", "bdrip", "480p", "480i", "576p", "576i", "720p", "720i", "1080p",
+        "1080i", "hrhd", "hrhdtv", "hddvd", "bluray", "blueray", "x264", "h264", "xvid", "xvidvd", "xxx", "www.www", "cd1", "cd2", "cd3", "cd4",
+        "cd5", "cd6", "cd7", "cd8", "cd9", "dvd1", "dvd2", "dvd3", "dvd4", "dvd5", "dvd6", "dvd7", "dvd8", "dvd9" };
+
+    if (filename == null || filename.isEmpty()) {
+      LOGGER.warn("Filename empty?!");
+      return "";
+    }
+
+    // remove extension (if found) and split
+    String fname = filename.replaceFirst("\\.\\w{2,4}$", "");
+    String[] s = fname.split("[() .-]");
+    int firstFoundStopwordPosition = s.length;
+
+    // iterate over all splitted items
+    for (int i = 0; i < s.length; i++) {
+      // search for stopword position
+      if (s[i] != null && !s[i].isEmpty()) {
+        for (String stop : stopwords) {
+          if (s[i].toLowerCase().equals(stop)) {
+            // remember lowest position
+            if (i < firstFoundStopwordPosition) {
+              firstFoundStopwordPosition = i;
+            }
+          }
+        }
+      }
+    }
+
+    // if we have at least 3 tokens, and the last one is a 4 digit, assume year
+    // and remove
+    if (firstFoundStopwordPosition > 3 && s[firstFoundStopwordPosition - 1].matches("\\d{4}")) {
+      LOGGER.debug("removed last token - seems to be year");
+      firstFoundStopwordPosition--;
+    }
+
+    // rebuild string
+    String ret = "";
+    for (int i = 0; i < firstFoundStopwordPosition; i++) {
+      if (!s[i].isEmpty()) {
+        ret = ret + s[i] + " ";
+      }
+    }
+    LOGGER.debug("Movie title should be: \"" + ret.trim() + "\"");
+    return ret.trim();
+  }
 
   /**
    * return a 2 element array. 0 = title; 1=date
---
+++ test/org/tinymediamanager/core/ParserUtilsTest.java
@@ -0,0 +1,21 @@
+package org.tinymediamanager.core;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.tinymediamanager.scraper.util.ParserUtils;
+
+public class ParserUtilsTest {
+
+  @Test
+  public void getTitle() {
+    File f = new File("D:\\filme");
+    File[] fileArray = f.listFiles();
+    for (File file : fileArray) {
+      if (!file.isDirectory()) {
+        System.out.println(ParserUtils.detectCleanMoviename(file.getName()));
+      }
+    }
+  }
+
+}
