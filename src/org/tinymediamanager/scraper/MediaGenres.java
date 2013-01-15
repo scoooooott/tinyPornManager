f66030e0cbb2156cd0086475aa5fcf7e8cb3133f
Myron <myron0815@gmx.net>
2013-01-15 17:24:00 +0000

    added more genres/translations based on values used by imdb/tmdb/ofdb

--- src/org/tinymediamanager/scraper/MediaGenres.java
+++ src/org/tinymediamanager/scraper/MediaGenres.java
@@ -11,47 +11,49 @@ public enum MediaGenres {
   /** The action. */
   ACTION("Action", new String[] {}),
   /** The adventure. */
-  ADVENTURE("Adventure", new String[] {}),
+  ADVENTURE("Adventure", new String[] { "Abenteuer" }),
   /** The animation. */
-  ANIMATION("Animation", new String[] {}),
+  ANIMATION("Animation", new String[] { "Anime", "Zeichentrick" }),
+  /** The animal. */
+  ANIMAL("Animal", new String[] { "Tierfilm" }),
   /** The Biography. */
-  BIOGRAPHY("Biography", new String[] {}),
+  BIOGRAPHY("Biography", new String[] { "Biographie" }),
   /** The comedy. */
-  COMEDY("Comedy", new String[] {}),
+  COMEDY("Comedy", new String[] { "Komödie" }),
   /** The crime. */
-  CRIME("Crime", new String[] {}),
+  CRIME("Crime", new String[] { "Krimi" }),
   /** The disaster. */
-  DISASTER("Disaster", new String[] {}),
+  DISASTER("Disaster", new String[] { "Katastrophen", "Katastrophenfilm" }),
   /** The documentary. */
-  DOCUMENTARY("Documentary", new String[] {}),
+  DOCUMENTARY("Documentary", new String[] { "Dokumentation", "Mondo" }),
   /** The drama. */
   DRAMA("Drama", new String[] {}),
   /** The eastern. */
   EASTERN("Eastern", new String[] {}),
   /** The erotic. */
-  EROTIC("Erotic", new String[] {}),
+  EROTIC("Erotic", new String[] { "Erotik", "Sex", "Adult" }),
   /** The family. */
-  FAMILY("Family", new String[] {}),
+  FAMILY("Family", new String[] { "Kinder-/Familienfilm", "Familie" }),
   /** The fan film. */
-  FAN_FILM("Fan Film", new String[] {}),
+  FAN_FILM("Fan Film", new String[] { "Fan-Film" }),
   /** The fantasy. */
   FANTASY("Fantasy", new String[] {}),
   /** The film noir. */
-  FILM_NOIR("Film Noir", new String[] { "Film-Noir" }),
+  FILM_NOIR("Film Noir", new String[] { "Film-Noir", "Neo-noir" }),
   /** The foreign. */
   FOREIGN("Foreign", new String[] {}),
   /** The game show. */
   GAME_SHOW("Gameshow", new String[] { "Game-Show" }),
   /** The history. */
-  HISTORY("History", new String[] {}),
+  HISTORY("History", new String[] { "Historienfilm", "Geschichte", "Historie" }),
   /** The holiday. */
   HOLIDAY("Holiday", new String[] {}),
   /** The horror. */
-  HORROR("Horror", new String[] {}),
+  HORROR("Horror", new String[] { "Splatter", "Grusel" }),
   /** The indie. */
-  INDIE("Indie", new String[] {}),
+  INDIE("Indie", new String[] { "Experimentalfilm", "Amateur" }),
   /** The music. */
-  MUSIC("Music", new String[] {}),
+  MUSIC("Music", new String[] { "Musikfilm", "Musik" }),
   /** The musical. */
   MUSICAL("Musical", new String[] {}),
   /** The mystery. */
@@ -59,33 +61,37 @@ public enum MediaGenres {
   /** The neo noir. */
   NEO_NOIR("Neo Noir", new String[] {}),
   /** The news. */
-  NEWS("News", new String[] {}),
+  NEWS("News", new String[] { "Nachrichten" }),
   /** The reality tv. */
   REALITY_TV("Reality TV", new String[] { "Reality-TV" }),
   /** The road movie. */
   ROAD_MOVIE("Road Movie", new String[] {}),
   /** The romance. */
-  ROMANCE("Romance", new String[] {}),
+  ROMANCE("Romance", new String[] { "Liebe/Romantik", "Romanze", "Lovestory" }),
   /** The science fiction. */
-  SCIENCE_FICTION("Science Fiction", new String[] { "Sci-Fi" }),
+  SCIENCE_FICTION("Science Fiction", new String[] { "Sci-Fi", "Science-Fiction" }),
+  /** The (tv) series. */
+  SERIES("Series", new String[] { "Serie", "TV-Serie", "TV-Mini-Serie" }),
   /** The short. */
-  SHORT("Short", new String[] {}),
+  SHORT("Short", new String[] { "Kurzfilm" }),
+  /** The silent ones. */
+  SILENT_MOVIE("Silent Movie", new String[] { "Stummfilm" }),
   /** The sport. */
-  SPORT("Sport", new String[] {}),
+  SPORT("Sport", new String[] { "Kampfsport" }),
   /** The sporting event. */
-  SPORTING_EVENT("Sporting Event", new String[] {}),
+  SPORTING_EVENT("Sporting Event", new String[] { "Sportereignis" }),
   /** The sports film. */
-  SPORTS_FILM("Sports Film", new String[] {}),
+  SPORTS_FILM("Sports Film", new String[] { "Sportfilm", "Sport Film" }),
   /** The suspense. */
   SUSPENSE("Suspense", new String[] {}),
   /** The talk show. */
   TALK_SHOW("Talk show", new String[] { "Talk-Show" }),
   /** The tv movie. */
-  TV_MOVIE("TV Movie", new String[] {}),
+  TV_MOVIE("TV Movie", new String[] { "TV-Film", "TV-Pilotfilm" }),
   /** The thriller. */
   THRILLER("Thriller", new String[] {}),
   /** The war. */
-  WAR("War", new String[] {}),
+  WAR("War", new String[] { "Krieg", "Kriegsfilm" }),
   /** The western. */
   WESTERN("Western", new String[] {});
 
