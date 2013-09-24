b950b664bfcdb5d02c31d2bb45674fe84bddc17b
Manuel Laggner <manuel.laggner@gmail.com>
2013-09-24 08:10:25 +0000

    fix: change working directory in cmd shellscript

--- AppBundler/tinyMediaManagerCMD.sh
+++ AppBundler/tinyMediaManagerCMD.sh
@@ -6,4 +6,6 @@ if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   PARAMS=-Djna.nosys=true
 fi
 
+# change to the tmm directory
+cd "${0%/*}"
 java -Djava.awt.headless=true -Xms64m -Xmx512m -Xss512k $PARAMS -jar tmm.jar $1 $2 $3 $4 $5
