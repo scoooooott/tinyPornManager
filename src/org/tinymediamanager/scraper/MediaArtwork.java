c02d1c5803a4130c2b145f61b1b1b466d998bda2
Manuel Laggner <manuel.laggner@gmail.com>
2014-05-05 05:56:46 +0000

    revert commit of MediaArtwork.java

--- src/org/tinymediamanager/scraper/MediaArtwork.java
+++ src/org/tinymediamanager/scraper/MediaArtwork.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 - 2014 Manuel Laggner
+ * Copyright 2012 - 2013 Manuel Laggner
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,6 @@ import java.util.ResourceBundle;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
-import org.tinymediamanager.core.MediaFileType;
 import org.tinymediamanager.ui.UTF8Control;
 
 /**
@@ -39,95 +38,22 @@ public class MediaArtwork {
    * 
    * @author Manuel Laggner
    */
+
   public enum MediaArtworkType {
-    // @formatter:off
+    /** The background. */
     BACKGROUND,
+    /** The banner. */
     BANNER,
+    /** The poster. */
     POSTER,
+    /** The actor. */
     ACTOR,
+    /** The season. */
     SEASON,
+    /** The thumb. */
     THUMB,
-    CLEARART,
-    DISC,
-    LOGO,
+    /** All. */
     ALL;
-    // @formatter:on
-
-    /**
-     * get the corresponding media file type for the artwork type
-     * 
-     * @param artworkType
-     *          the artwork type
-     * @return the media file type
-     */
-    public static MediaFileType getMediaFileType(MediaArtworkType artworkType) {
-      switch (artworkType) {
-        case BACKGROUND:
-          return MediaFileType.FANART;
-
-        case BANNER:
-          return MediaFileType.BANNER;
-
-        case POSTER:
-          return MediaFileType.POSTER;
-
-        case SEASON:
-          return MediaFileType.SEASON_POSTER;
-
-        case THUMB:
-          return MediaFileType.THUMB;
-
-        case CLEARART:
-          return MediaFileType.CLEARART;
-
-        case LOGO:
-          return MediaFileType.LOGO;
-
-        case DISC:
-          return MediaFileType.DISC;
-
-        default:
-          return MediaFileType.GRAPHIC;
-      }
-    }
-
-    /**
-     * get the corresponding artwork type for the file type
-     * 
-     * @param fileType
-     *          the file type
-     * @return the artwork type
-     */
-    public static MediaArtworkType getMediaArtworkType(MediaFileType fileType) {
-      switch (fileType) {
-        case FANART:
-          return MediaArtworkType.BACKGROUND;
-
-        case BANNER:
-          return MediaArtworkType.BANNER;
-
-        case POSTER:
-          return MediaArtworkType.POSTER;
-
-        case SEASON_POSTER:
-          return MediaArtworkType.SEASON;
-
-        case THUMB:
-          return MediaArtworkType.THUMB;
-
-        case CLEARART:
-          return MediaArtworkType.CLEARART;
-
-        case LOGO:
-          return MediaArtworkType.LOGO;
-
-        case DISC:
-          return MediaArtworkType.DISC;
-
-        default:
-          throw new IllegalStateException();
-      }
-    }
   }
 
   /**
