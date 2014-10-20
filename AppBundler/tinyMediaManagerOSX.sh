8686facac4dfa63423a5933e3e789ecd79dc343d
Manuel Laggner <manuel.laggner@gmail.com>
2014-10-20 18:36:52 +0000

    changed OSX shellscript to take java7/8 if available

--- AppBundler/tinyMediaManagerOSX.sh
+++ AppBundler/tinyMediaManagerOSX.sh
@@ -15,23 +15,22 @@
 export LC_ALL="en_US.UTF-8"
 
 # search for the right JVM
-if [ -n "$JAVA_HOME" ]; then
-  JAVACMD="$JAVA_HOME/bin/java"
+if [ -x "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin/java" ]; then
+  JAVA_HOME="/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home"
+  export JAVA_HOME
 elif [ -x /usr/libexec/java_home ]; then
-  JAVACMD="`/usr/libexec/java_home`/bin/java"
-else
-  JAVACMD="/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin/java"
+  JAVA_HOME="/usr/libexec/java_home"
+  export JAVA_HOME  
 fi
+JAVACMD="$JAVA_HOME/bin/java"
 
 # have a look if we need to launch the updater or tmm directly
 if [ -f tmm.jar ]; then
   ARGS="-Dsilent=noupdate"
-else
-  ARGS="-Xdock:name=`tinyMediaManager updater`"
 fi
 
 ARGS="$ARGS -Djava.net.preferIPv4Stack=true"
 
 # execute it :)
-exec "$JAVACMD" $ARGS -jar getdown.jar .      
+exec "$JAVACMD" "$ARGS" -jar getdown.jar .      
 
