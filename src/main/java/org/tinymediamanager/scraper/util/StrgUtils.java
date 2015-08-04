19da4bf5ac9713fc6120282357e1e5a3cebb666c
myron0815 <myron0815@gmx.net>
2015-08-04 17:34:04 +0200

    getLongestString()

--- src/main/java/org/tinymediamanager/scraper/util/StrgUtils.java
+++ src/main/java/org/tinymediamanager/scraper/util/StrgUtils.java
@@ -350,4 +350,14 @@ public class StrgUtils {
     return sb.toString();
   }
 
-}
+  public static String getLongestString(String[] array) {
+    int maxLength = 0;
+    String longestString = null;
+    for (String s : array) {
+        if (s.length() > maxLength) {
+            maxLength = s.length();
+            longestString = s;
+        }
+    }
+    return longestString;
+}}
