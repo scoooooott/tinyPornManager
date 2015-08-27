e2e492f7ccae90136c62e93096d545309a421caa
myron0815 <myron0815@gmx.net>
2015-08-27 19:15:33 +0200

    use ResourceBundle.newBundle() from current JDK7
    this seems to resolve the language loading problems

--- src/main/java/org/tinymediamanager/ui/UTF8Control.java
+++ src/main/java/org/tinymediamanager/ui/UTF8Control.java
@@ -20,6 +20,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.security.AccessController;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
 import java.util.Locale;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
@@ -34,37 +37,86 @@ import java.util.ResourceBundle.Control;
  */
 public class UTF8Control extends Control {
   @Override
-  public ResourceBundle newBundle(String baseName, Locale locale, String format, ClassLoader loader, boolean reload) throws IllegalAccessException,
-      InstantiationException, IOException {
+  public ResourceBundle newBundle(String baseName, Locale locale, String format, ClassLoader loader, boolean reload)
+      throws IllegalAccessException, InstantiationException, IOException {
     // The below is a copy of the default implementation.
     String bundleName = toBundleName(baseName, locale);
-    String resourceName = toResourceName(bundleName, "properties");
     ResourceBundle bundle = null;
-    InputStream stream = null;
-    if (reload) {
-      URL url = loader.getResource(resourceName);
-      if (url != null) {
-        URLConnection connection = url.openConnection();
-        if (connection != null) {
-          connection.setUseCaches(false);
-          stream = connection.getInputStream();
+    if (format.equals("java.class")) {
+      try {
+        Class<? extends ResourceBundle> bundleClass = (Class<? extends ResourceBundle>) loader.loadClass(bundleName);
+
+        // If the class isn't a ResourceBundle subclass, throw a
+        // ClassCastException.
+        if (ResourceBundle.class.isAssignableFrom(bundleClass)) {
+          bundle = bundleClass.newInstance();
+        }
+        else {
+          throw new ClassCastException(bundleClass.getName() + " cannot be cast to ResourceBundle");
+        }
+      }
+      catch (ClassNotFoundException e) {
+      }
+    }
+    else if (format.equals("java.properties")) {
+      final String resourceName = toResourceName0(bundleName, "properties");
+      if (resourceName == null) {
+        return bundle;
+      }
+      final ClassLoader classLoader = loader;
+      final boolean reloadFlag = reload;
+      InputStream stream = null;
+      try {
+        stream = AccessController.doPrivileged(new PrivilegedExceptionAction<InputStream>() {
+          public InputStream run() throws IOException {
+            InputStream is = null;
+            if (reloadFlag) {
+              URL url = classLoader.getResource(resourceName);
+              if (url != null) {
+                URLConnection connection = url.openConnection();
+                if (connection != null) {
+                  // Disable caches to get fresh data for
+                  // reloading.
+                  connection.setUseCaches(false);
+                  is = connection.getInputStream();
+                }
+              }
+            }
+            else {
+              is = classLoader.getResourceAsStream(resourceName);
+            }
+            return is;
+          }
+        });
+      }
+      catch (PrivilegedActionException e) {
+        throw (IOException) e.getException();
+      }
+      if (stream != null) {
+        try {
+          // Only this line is changed to make it to read properties files as UTF-8.
+          // bundle = new PropertyResourceBundle(stream);
+          bundle = new PropertyResourceBundle(new InputStreamReader(stream, "UTF-8"));
+        }
+        finally {
+          stream.close();
         }
       }
     }
     else {
-      stream = loader.getResourceAsStream(resourceName);
+      throw new IllegalArgumentException("unknown format: " + format);
     }
 
-    if (stream != null) {
-      try {
-        // Only this line is changed to make it to read properties files as
-        // UTF-8.
-        bundle = new PropertyResourceBundle(new InputStreamReader(stream, "UTF-8"));
-      }
-      finally {
-        stream.close();
-      }
-    }
     return bundle;
+  }
+
+  private String toResourceName0(String bundleName, String suffix) {
+    // application protocol check
+    if (bundleName.contains("://")) {
+      return null;
+    }
+    else {
+      return toResourceName(bundleName, suffix);
+    }
   }
 }
