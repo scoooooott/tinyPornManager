8967ff8a095a5f2c92a685c89ce57b9b26e1c52c
myron0815 <myron0815@gmx.net>
2015-09-21 21:20:11 +0200

    rework logging

--- src/test/java/org/tinymediamanager/scraper/fanarttv/FanartTvMetadataProviderTest.java
+++ src/test/java/org/tinymediamanager/scraper/fanarttv/FanartTvMetadataProviderTest.java
@@ -15,51 +15,27 @@
  */
 package org.tinymediamanager.scraper.fanarttv;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
-import java.util.logging.LogManager;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.tinymediamanager.scraper.mediaprovider.IMovieArtworkProvider;
 import org.tinymediamanager.scraper.MediaArtwork;
 import org.tinymediamanager.scraper.MediaLanguages;
 import org.tinymediamanager.scraper.MediaScrapeOptions;
 import org.tinymediamanager.scraper.MediaType;
+import org.tinymediamanager.scraper.mediaprovider.IMovieArtworkProvider;
 
 public class FanartTvMetadataProviderTest {
-  private static final String CRLF = "\n";
 
-  @BeforeClass
-  public static void setUp() {
-    StringBuilder config = new StringBuilder("handlers = java.util.logging.ConsoleHandler\n");
-    config.append(".level = ALL").append(CRLF);
-    config.append("java.util.logging.ConsoleHandler.level = ALL").append(CRLF);
-    // Only works with Java 7 or later
-    config.append("java.util.logging.SimpleFormatter.format = [%1$tH:%1$tM:%1$tS %4$6s] %2$s - %5$s %6$s%n").append(CRLF);
-    // Exclude http logging
-    config.append("sun.net.www.protocol.http.HttpURLConnection.level = OFF").append(CRLF);
-    InputStream ins = new ByteArrayInputStream(config.toString().getBytes());
-    try {
-      LogManager.getLogManager().readConfiguration(ins);
-    }
-    catch (IOException ignored) {
-    }
-  }
-  
   @Test
-  public void testFanartTvMetadataProvider(){
+  public void testFanartTvMetadataProvider() {
     IMovieArtworkProvider artworkProvider;
 
     /**
      * Test movie artwork
      */
-    try{
+    try {
       artworkProvider = new FanartTvMetadataProvider("");
 
       MediaScrapeOptions options = new MediaScrapeOptions(MediaType.MOVIE);
@@ -74,7 +50,8 @@ public class FanartTvMetadataProviderTest {
       assertThat(images.get(0).getImageSizes()).isNotNull().isNotEmpty();
       assertThat(images.get(0).getImageSizes().get(0).getHeight()).isGreaterThan(images.get(0).getImageSizes().get(0).getWidth());
       assertThat(images.get(0).getImageSizes().get(0).getWidth()).isLessThan(images.get(0).getImageSizes().get(0).getHeight());
-    } catch (Exception e){
+    }
+    catch (Exception e) {
       e.printStackTrace();
       fail(e.getMessage());
     }
