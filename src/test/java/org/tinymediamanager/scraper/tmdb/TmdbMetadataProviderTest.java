2adb1c001eb43b25fdef6165b5015089b78acd44
myron0815 <myron0815@gmx.net>
2015-09-21 21:24:55 +0200

    rework logging

--- src/test/java/org/tinymediamanager/scraper/tmdb/TmdbMetadataProviderTest.java
+++ src/test/java/org/tinymediamanager/scraper/tmdb/TmdbMetadataProviderTest.java
@@ -4,18 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 import static org.junit.Assert.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.List;
-import java.util.logging.LogManager;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.tinymediamanager.scraper.CountryCode;
-import org.tinymediamanager.scraper.mediaprovider.IMovieMetadataProvider;
-import org.tinymediamanager.scraper.mediaprovider.ITvShowMetadataProvider;
 import org.tinymediamanager.scraper.MediaCastMember.CastType;
 import org.tinymediamanager.scraper.MediaEpisode;
 import org.tinymediamanager.scraper.MediaLanguages;
@@ -25,26 +18,10 @@ import org.tinymediamanager.scraper.MediaSearchOptions;
 import org.tinymediamanager.scraper.MediaSearchOptions.SearchParam;
 import org.tinymediamanager.scraper.MediaSearchResult;
 import org.tinymediamanager.scraper.MediaType;
+import org.tinymediamanager.scraper.mediaprovider.IMovieMetadataProvider;
+import org.tinymediamanager.scraper.mediaprovider.ITvShowMetadataProvider;
 
 public class TmdbMetadataProviderTest {
-  private static final String CRLF = "\n";
-
-  @BeforeClass
-  public static void setUp() {
-    StringBuilder config = new StringBuilder("handlers = java.util.logging.ConsoleHandler\n");
-    config.append(".level = ALL").append(CRLF);
-    config.append("java.util.logging.ConsoleHandler.level = ALL").append(CRLF);
-    // Only works with Java 7 or later
-    config.append("java.util.logging.SimpleFormatter.format = [%1$tH:%1$tM:%1$tS %4$6s] %2$s - %5$s %6$s%n").append(CRLF);
-    // Exclude http logging
-    config.append("sun.net.www.protocol.http.HttpURLConnection.level = OFF").append(CRLF);
-    InputStream ins = new ByteArrayInputStream(config.toString().getBytes());
-    try {
-      LogManager.getLogManager().readConfiguration(ins);
-    }
-    catch (IOException ignored) {
-    }
-  }
 
   @Test
   public void testMovieSearch() {
