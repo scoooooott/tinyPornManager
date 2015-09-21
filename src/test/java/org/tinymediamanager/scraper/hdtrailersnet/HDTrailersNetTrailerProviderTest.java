17339027787a50f2e9d4d09d5d9d9f8872756563
myron0815 <myron0815@gmx.net>
2015-09-21 21:21:03 +0200

    rework logging

--- src/test/java/org/tinymediamanager/scraper/hdtrailersnet/HDTrailersNetTrailerProviderTest.java
+++ src/test/java/org/tinymediamanager/scraper/hdtrailersnet/HDTrailersNetTrailerProviderTest.java
@@ -1,15 +1,9 @@
 package org.tinymediamanager.scraper.hdtrailersnet;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
-import java.util.logging.LogManager;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.tinymediamanager.scraper.MediaMetadata;
 import org.tinymediamanager.scraper.MediaScrapeOptions;
@@ -18,25 +12,6 @@ import org.tinymediamanager.scraper.MediaType;
 import org.tinymediamanager.scraper.mediaprovider.IMovieTrailerProvider;
 
 public class HDTrailersNetTrailerProviderTest {
-  private static final String CRLF = "\n";
-
-  @BeforeClass
-  public static void setUp() {
-    StringBuilder config = new StringBuilder("handlers = java.util.logging.ConsoleHandler\n");
-    config.append(".level = ALL").append(CRLF);
-    config.append("java.util.logging.ConsoleHandler.level = ALL").append(CRLF);
-    // Only works with Java 7 or later
-    config.append("java.util.logging.SimpleFormatter.format = [%1$tH:%1$tM:%1$tS %4$6s] %2$s - %5$s %6$s%n")
-        .append(CRLF);
-    // Exclude http logging
-    config.append("sun.net.www.protocol.http.HttpURLConnection.level = OFF").append(CRLF);
-    InputStream ins = new ByteArrayInputStream(config.toString().getBytes());
-    try {
-      LogManager.getLogManager().readConfiguration(ins);
-    }
-    catch (IOException ignored) {
-    }
-  }
 
   @Test
   public void testScrapeTrailer() {
@@ -52,7 +27,7 @@ public class HDTrailersNetTrailerProviderTest {
 
       List<MediaTrailer> trailers = mp.getTrailers(options);
       assertThat(trailers).isNotNull().isNotEmpty();
-      
+
       MediaTrailer trailer = trailers.get(0);
       assertThat(trailer.getName()).isNotEmpty();
       assertThat(trailer.getUrl()).isNotEmpty();
