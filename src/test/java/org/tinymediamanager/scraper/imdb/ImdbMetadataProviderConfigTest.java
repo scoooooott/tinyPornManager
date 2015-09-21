b51e8d59c0c46e588f7f5244c79fd3fdb52dff19
myron0815 <myron0815@gmx.net>
2015-09-21 21:16:39 +0200

    remove weird logging, fix test order
    Tests are not guranteed to be executed in order :| so changing the
    write/load test to a single test 8since it always fails here)

--- src/test/java/org/tinymediamanager/scraper/imdb/ImdbMetadataProviderConfigTest.java
+++ src/test/java/org/tinymediamanager/scraper/imdb/ImdbMetadataProviderConfigTest.java
@@ -1,46 +1,24 @@
 package org.tinymediamanager.scraper.imdb;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.logging.LogManager;
-
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ImdbMetadataProviderConfigTest {
-  private static final String CRLF = "\n";
-
-  @BeforeClass
-  public static void setUp() {
-    StringBuilder config = new StringBuilder("handlers = java.util.logging.ConsoleHandler\n");
-    config.append(".level = ALL").append(CRLF);
-    config.append("java.util.logging.ConsoleHandler.level = ALL").append(CRLF);
-    // Only works with Java 7 or later
-    config.append("java.util.logging.SimpleFormatter.format = [%1$tH:%1$tM:%1$tS %4$6s] %2$s - %5$s %6$s%n").append(CRLF);
-    // Exclude http logging
-    config.append("sun.net.www.protocol.http.HttpURLConnection.level = OFF").append(CRLF);
-    InputStream ins = new ByteArrayInputStream(config.toString().getBytes());
-    try {
-      LogManager.getLogManager().readConfiguration(ins);
-    }
-    catch (IOException ignored) {
-    }
-  }
 
   @Test
-  public void writeConfig() {
+  public void testConfig() {
     ImdbMetadataProviderConfig config = ImdbMetadataProviderConfig.SETTINGS;
     config.useTmdb = true;
     config.scrapeCollectionInfo = true;
     config.save();
+
+    config.filterUnwantedCategories = false;
+    config.scrapeCollectionInfo = false;
+    config.useTmdb = false;
+
+    config = ImdbMetadataProviderConfig.SETTINGS;
+    Assert.assertTrue(config.useTmdb);
+    Assert.assertTrue(config.scrapeCollectionInfo);
   }
 
-  @Test
-  public void loadConfig() {
-    ImdbMetadataProviderConfig config = ImdbMetadataProviderConfig.SETTINGS;
-    Assert.assertEquals(true, config.useTmdb);
-    Assert.assertEquals(true, config.scrapeCollectionInfo);
-  }
 }
--- src/test/java/org/tinymediamanager/scraper/imdb/ImdbMetadataProviderTest.java
+++ src/test/java/org/tinymediamanager/scraper/imdb/ImdbMetadataProviderTest.java
@@ -2,17 +2,12 @@ package org.tinymediamanager.scraper.imdb;
 
 import static org.junit.Assert.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.logging.LogManager;
 
 import org.apache.commons.lang3.StringUtils;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.tinymediamanager.scraper.CountryCode;
 import org.tinymediamanager.scraper.MediaArtwork;
@@ -29,24 +24,6 @@ import org.tinymediamanager.scraper.MediaType;
 import org.tinymediamanager.scraper.http.ProxySettings;
 
 public class ImdbMetadataProviderTest {
-  private static final String CRLF = "\n";
-
-  @BeforeClass
-  public static void setUp() {
-    StringBuilder config = new StringBuilder("handlers = java.util.logging.ConsoleHandler\n");
-    config.append(".level = ALL").append(CRLF);
-    config.append("java.util.logging.ConsoleHandler.level = ALL").append(CRLF);
-    // Only works with Java 7 or later
-    config.append("java.util.logging.SimpleFormatter.format = [%1$tH:%1$tM:%1$tS %4$6s] %2$s - %5$s %6$s%n").append(CRLF);
-    // Exclude http logging
-    config.append("sun.net.www.protocol.http.HttpURLConnection.level = OFF").append(CRLF);
-    InputStream ins = new ByteArrayInputStream(config.toString().getBytes());
-    try {
-      LogManager.getLogManager().readConfiguration(ins);
-    }
-    catch (IOException ignored) {
-    }
-  }
 
   @Test
   public void testMovieSearch() {
