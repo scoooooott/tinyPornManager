be6a643c660eb9b3cb0b17e13ae73c01d43cd526
Manuel Laggner <manuel.laggner@gmail.com>
2016-08-29 10:41:42 +0200

    migrated AboutDialog to miglayout

--- src/main/java/org/tinymediamanager/ui/dialogs/AboutDialog.java
+++ src/main/java/org/tinymediamanager/ui/dialogs/AboutDialog.java
@@ -36,8 +36,8 @@ import org.tinymediamanager.Globals;
 import org.tinymediamanager.ReleaseInfo;
 import org.tinymediamanager.core.License;
 import org.tinymediamanager.core.Message;
-import org.tinymediamanager.core.Message.MessageLevel;
 import org.tinymediamanager.core.MessageManager;
+import org.tinymediamanager.core.Message.MessageLevel;
 import org.tinymediamanager.ui.IconManager;
 import org.tinymediamanager.ui.TmmFontHelper;
 import org.tinymediamanager.ui.TmmUIHelper;
@@ -45,11 +45,7 @@ import org.tinymediamanager.ui.UTF8Control;
 import org.tinymediamanager.ui.components.LinkLabel;
 import org.tinymediamanager.ui.images.Logo;
 
-import com.jgoodies.forms.factories.FormFactory;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.FormSpecs;
-import com.jgoodies.forms.layout.RowSpec;
+import net.miginfocom.swing.MigLayout;
 
 /**
  * The Class AboutDialog.
@@ -58,9 +54,7 @@ import com.jgoodies.forms.layout.RowSpec;
  */
 public class AboutDialog extends TmmDialog {
   private static final long           serialVersionUID = 2298570526828925319L;
-  /**
-   * @wbp.nls.resourceBundle messages
-   */
+  /** @wbp.nls.resourceBundle messages */
   private static final ResourceBundle BUNDLE           = ResourceBundle.getBundle("messages", new UTF8Control());                                                     //$NON-NLS-1$
   private static final Logger         LOGGER           = LoggerFactory.getLogger(AboutDialog.class);
   private static final String         TRANSLATORS      = "Joostzilla, Zagoslav, zbynek.fiala, roliverosc, roandr, Andrey Gorodnov, julienbloch, nerve, carlosmarchi, "
@@ -73,59 +67,46 @@ public class AboutDialog extends TmmDialog {
 
   public AboutDialog() {
     super(BUNDLE.getString("tmm.about"), "aboutDialog"); //$NON-NLS-1$
-    setBounds(100, 100, 655, 500);
     getContentPane().setLayout(new BorderLayout());
     contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
     getContentPane().add(contentPanel, BorderLayout.CENTER);
-    contentPanel.setLayout(new FormLayout(
-        new ColumnSpec[] { FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC, FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC,
-            FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode("300dlu:grow"), FormSpecs.RELATED_GAP_COLSPEC, },
-        new RowSpec[] { FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
-            FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
-            FormSpecs.RELATED_GAP_ROWSPEC, RowSpec.decode("max(25px;min)"), FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
-            FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
-            FormSpecs.LABEL_COMPONENT_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.LABEL_COMPONENT_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
-            FormSpecs.LABEL_COMPONENT_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.LABEL_COMPONENT_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
-            FormSpecs.LABEL_COMPONENT_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.LABEL_COMPONENT_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
-            FormSpecs.LABEL_COMPONENT_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.LABEL_COMPONENT_GAP_ROWSPEC,
-            RowSpec.decode("default:grow"), }));
+    contentPanel.setLayout(new MigLayout("", "[][pref!][][pref!][grow]", "[][6px][][pref!][][6px][][][][][][][][]"));
     {
       JLabel lblLogo = new JLabel("");
       lblLogo.setIcon(new Logo(96));
-      contentPanel.add(lblLogo, "2, 2, 1, 9, default, top");
+      contentPanel.add(lblLogo, "cell 0 0 1 7,alignx left,aligny top");
     }
     {
       JLabel lblTinymediamanager = new JLabel("tinyMediaManager"); //$NON-NLS-1$
       TmmFontHelper.changeFont(lblTinymediamanager, 1.5, Font.BOLD);
-      contentPanel.add(lblTinymediamanager, "4, 2, 3, 1, center, default");
+      contentPanel.add(lblTinymediamanager, "cell 2 0 3 1,alignx center");
     }
     {
       JLabel lblByManuel = new JLabel("\u00A9 2012 - 2015 by Manuel Laggner"); //$NON-NLS-1$
-      contentPanel.add(lblByManuel, "4, 4, 3, 1, center, default");
+      contentPanel.add(lblByManuel, "cell 2 2 3 1,alignx center");
     }
     {
       if (Globals.isDonator()) {
         Properties p = License.decrypt();
-        // p.list(System.out);
         JLabel lblRegged = new JLabel(BUNDLE.getString("tmm.registeredto") + " " + p.getProperty("user")); //$NON-NLS-1$
         TmmFontHelper.changeFont(lblRegged, 1.166, Font.BOLD);
-        contentPanel.add(lblRegged, "4, 6, 3, 1, center, default");
+        contentPanel.add(lblRegged, "cell 4 4");
       }
     }
     {
       JLabel lblVersion = new JLabel(BUNDLE.getString("tmm.version") + ": " + ReleaseInfo.getRealVersion()); //$NON-NLS-1$
-      contentPanel.add(lblVersion, "6, 8, left, top");
+      contentPanel.add(lblVersion, "cell 2 4");
     }
     {
       JLabel lblBuild = new JLabel(BUNDLE.getString("tmm.builddate") + ": " + ReleaseInfo.getRealBuildDate());//$NON-NLS-1$
-      contentPanel.add(lblBuild, "6, 10, left, top");
+      contentPanel.add(lblBuild, "cell 2 6");
     }
     {
       JLabel lblHomepage = new JLabel(BUNDLE.getString("tmm.homepage")); //$NON-NLS-1$
-      contentPanel.add(lblHomepage, "2, 12, right, default");
+      contentPanel.add(lblHomepage, "cell 0 7,alignx right");
     }
     {
-      final LinkLabel lblHomepage = new LinkLabel("http://www.tinymediamanager.org/");
+      final LinkLabel lblHomepage = new LinkLabel("http://www.tinymediamanager.org/"); //$NON-NLS-1$
       lblHomepage.addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent arg0) {
           try {
@@ -134,45 +115,35 @@ public class AboutDialog extends TmmDialog {
           catch (Exception e) {
             LOGGER.error(e.getMessage());
             MessageManager.instance.pushMessage(
-                new Message(MessageLevel.ERROR, lblHomepage.getNormalText(), "message.erroropenurl", new String[] { ":", e.getLocalizedMessage() }));
+                new Message(MessageLevel.ERROR, lblHomepage.getNormalText(), "message.erroropenurl", new String[] { ":", e.getLocalizedMessage() })); //$NON-NLS-1$
           }
         }
       });
-      contentPanel.add(lblHomepage, "6, 12");
+      contentPanel.add(lblHomepage, "cell 2 7 3 1");
     }
     {
       JLabel lblThanksTo = new JLabel(BUNDLE.getString("tmm.thanksto")); //$NON-NLS-1$
-      contentPanel.add(lblThanksTo, "2, 16, right, default");
+      contentPanel.add(lblThanksTo, "cell 0 8,alignx right");
     }
     {
-      JLabel lblMyronForHelping = new JLabel("Myron for helping me with coding, scrapers, localization, setup, everything...");
-      contentPanel.add(lblMyronForHelping, "6, 16");
+      JLabel lblMyronForHelping = new JLabel("Myron for helping me with coding, scrapers, localization, setup, everything..."); //$NON-NLS-1$
+      contentPanel.add(lblMyronForHelping, "cell 2 8 3 1");
     }
     {
-      JLabel lblXysm = new JLabel("xysm for excessive testing and lots of feedback");
-      contentPanel.add(lblXysm, "6, 18");
+      JLabel lblXysm = new JLabel("xysm for excessive testing and lots of feedback"); //$NON-NLS-1$
+      contentPanel.add(lblXysm, "cell 2 9 3 1");
     }
     {
-      JLabel lblXzener = new JLabel("Xzener for genre images");
-      contentPanel.add(lblXzener, "6, 20");
-    }
-    {
-      JLabel lblMatthewSandersFor = new JLabel("Matthew Sanders for the cool export templates");
-      contentPanel.add(lblMatthewSandersFor, "6, 22");
-    }
-    {
-      JLabel lblLblakya = new JLabel("Akya for the reworked logo");
-      contentPanel.add(lblLblakya, "6, 24");
+      JLabel lblMatthewSandersFor = new JLabel("Matthew Sanders for the cool export templates"); //$NON-NLS-1$
+      contentPanel.add(lblMatthewSandersFor, "cell 2 10 3 1");
     }
     {
       JPanel panelTranslators = new JPanel();
-      contentPanel.add(panelTranslators, "6, 26, fill, fill");
-      panelTranslators
-          .setLayout(new FormLayout(new ColumnSpec[] { FormSpecs.DEFAULT_COLSPEC, FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode("200dlu:grow"), },
-              new RowSpec[] { RowSpec.decode("top:max(50dlu;pref)"), }));
+      contentPanel.add(panelTranslators, "cell 2 11 3 1,grow");
+      panelTranslators.setLayout(new MigLayout("insets 0", "[][300px:300px,grow]", "[]"));
       {
         JLabel lblTranslatorsT = new JLabel(BUNDLE.getString("tmm.translators")); //$NON-NLS-1$
-        panelTranslators.add(lblTranslatorsT, "1, 1, right, top");
+        panelTranslators.add(lblTranslatorsT, "cell 0 0,alignx right,aligny top");
       }
       {
         JTextPane tpTranslators = new JTextPane();
@@ -180,35 +151,28 @@ public class AboutDialog extends TmmDialog {
         tpTranslators.setEditable(false);
         tpTranslators.setOpaque(false);
         tpTranslators.setText(TRANSLATORS);
-        panelTranslators.add(tpTranslators, "3, 1, fill, fill");
+        panelTranslators.add(tpTranslators, "cell 1 0,growx,aligny top");
       }
     }
     {
-      JLabel lblLibs = new JLabel("The creators of all libs I've used");
-      contentPanel.add(lblLibs, "6, 28");
+      JLabel lblLibs = new JLabel("The creators of all libs I've used"); //$NON-NLS-1$
+      contentPanel.add(lblLibs, "cell 2 12 3 1");
     }
     {
-      JLabel lblTester = new JLabel("Everyone who tested and provided feedback");
-      contentPanel.add(lblTester, "6, 30");
+      JLabel lblTester = new JLabel("Everyone who tested and provided feedback"); //$NON-NLS-1$
+      contentPanel.add(lblTester, "cell 2 13 3 1");
     }
     {
       JPanel buttonPane = new JPanel();
       getContentPane().add(buttonPane, BorderLayout.SOUTH);
-      buttonPane.setLayout(
-          new FormLayout(new ColumnSpec[] { ColumnSpec.decode("default:grow"), FormFactory.BUTTON_COLSPEC, FormFactory.RELATED_GAP_COLSPEC, },
-              new RowSpec[] { FormFactory.LINE_GAP_ROWSPEC, RowSpec.decode("23px"), FormFactory.RELATED_GAP_ROWSPEC, }));
+      buttonPane.setLayout(new MigLayout("", "[grow][]", "[]"));
       {
         JButton okButton = new JButton();
         okButton.setAction(action);
-        buttonPane.add(okButton, "2, 2, fill, top");
+        buttonPane.add(okButton, "cell 1 0,growx");
         getRootPane().setDefaultButton(okButton);
       }
     }
-  }
-
-  @Override
-  public void pack() {
-
   }
 
   private class SwingAction extends AbstractAction {
