d0fa3b8b4d5f2631580ba5d206bd5782aacae2fa
Manuel Laggner <manuel.laggner@gmail.com>
2017-02-25 09:30:14 +0100

    feature: some utils for maps

---
+++ src/main/java/org/tinymediamanager/scraper/util/MapUtils.java
@@ -0,0 +1,78 @@
+/*
+ *
+ *  * Copyright 2012 - 2016 Manuel Laggner
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package org.tinymediamanager.scraper.util;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * The class MapUtils is a helper class, providing some special functions for maps
+ *
+ * @author Manuel Laggner
+ * @since 2.5
+ */
+public class MapUtils {
+  /**
+   * sort the given map by key and return a new map
+   * 
+   * @param map
+   *          the map to be sorted
+   * @return a new sorted map
+   */
+  public static <K extends Comparable<? super K>, V> Map<K, V> sortByKey(Map<K, V> map) {
+    List<Map.Entry<K, V>> list = new LinkedList<>(map.entrySet());
+    Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+      public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+        return (o1.getKey()).compareTo(o2.getKey());
+      }
+    });
+
+    Map<K, V> result = new LinkedHashMap<>();
+    for (Map.Entry<K, V> entry : list) {
+      result.put(entry.getKey(), entry.getValue());
+    }
+    return result;
+  }
+
+  /**
+   * sort the given map by value and return a new map
+   * 
+   * @param map
+   *          the map to be sorted
+   * @return a new sorted map
+   */
+  public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+    List<Map.Entry<K, V>> list = new LinkedList<>(map.entrySet());
+    Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+      public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+        return (o1.getValue()).compareTo(o2.getValue());
+      }
+    });
+
+    Map<K, V> result = new LinkedHashMap<>();
+    for (Map.Entry<K, V> entry : list) {
+      result.put(entry.getKey(), entry.getValue());
+    }
+    return result;
+  }
+}
