6580c68b4acc11054ce2fda5accd395e94d4e0b7
Myron Boyle <myron0815@gmx.net>
2017-10-04 13:44:35 +0200

    MediaSearchResult: implement equals() to filter duplicates

--- src/main/java/org/tinymediamanager/scraper/MediaSearchResult.java
+++ src/main/java/org/tinymediamanager/scraper/MediaSearchResult.java
@@ -358,6 +358,35 @@ public class MediaSearchResult implements Comparable<MediaSearchResult> {
     }
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((type == null) ? 0 : type.hashCode());
+    result = prime * result + ((url == null) ? 0 : url.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    MediaSearchResult other = (MediaSearchResult) obj;
+    if (type != other.type)
+      return false;
+    if (url == null) {
+      if (other.url != null)
+        return false;
+    }
+    else if (!url.equals(other.url))
+      return false;
+    return true;
+  }
+
   /**
    * <p>
    * Uses <code>ReflectionToStringBuilder</code> to generate a <code>toString</code> for the specified object.
