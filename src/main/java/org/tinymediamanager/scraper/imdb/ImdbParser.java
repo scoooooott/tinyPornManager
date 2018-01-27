576e13473b827889c7bdf111ea5555fa6e13aedf
myron0815 <myron0815@gmx.net>
2018-01-27 11:58:34 +0100

    fix Integer comnparison

--- src/main/java/org/tinymediamanager/scraper/imdb/ImdbParser.java
+++ src/main/java/org/tinymediamanager/scraper/imdb/ImdbParser.java
@@ -912,8 +912,8 @@ public abstract class ImdbParser {
   /**
    * Is i1 != i2 (when >0)
    */
-  private boolean yearDiffers(Integer i1, Integer i2) {
-    return i1 != null && i1 != 0 && i2 != null && i2 != 0 && i1 != i2;
+  private boolean yearDiffers(int i1, int i2) {
+    return i1 > 0 && i2 > 0 && i1 != i2;
   }
 
   protected Date parseDate(String dateAsSting) {
