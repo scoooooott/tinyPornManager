6a67e9aec133f0d6221e586ec063bf9d8eae14d1
Manuel Laggner <manuel.laggner@gmail.com>
2018-02-17 14:40:19 +0100

    fix: reverted last changelog generation changes

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm changelog.txt
 DATE=$(date +%F)
-git log --no-merges --format="%cd" --date=short --invert-grep --grep="Translated using Weblate" --since="2016-01-20 00:00:00" v3 | sort -u -r | while read DATE ; do
+git log --no-merges --format="%cd" --date=short --invert-grep --grep="Translated using Weblate" --since="2016-01-20 00:00:00" devel.. | sort -u -r | while read DATE ; do
   echo >> changelog.txt
   echo [$DATE] >> changelog.txt
-  GIT_PAGER=cat git log --no-merges --format=" * %s" --since="$DATE 00:00:00" --until="$DATE 23:59:59" --invert-grep --grep="Translated using Weblate" v3 >> changelog.txt
+  GIT_PAGER=cat git log --no-merges --format=" * %s" --since="$DATE 00:00:00" --until="$DATE 23:59:59" --invert-grep --grep="Translated using Weblate" devel.. >> changelog.txt
 done
