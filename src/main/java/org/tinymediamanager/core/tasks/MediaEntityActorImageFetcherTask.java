3078cac6e6f42f3b6a7fcfc2eced2e75d3a0394a
Manuel Laggner <manuel.laggner@gmail.com>
2018-10-16 09:42:43 +0200

    fix: downloading actor images with disabled image cache

--- src/main/java/org/tinymediamanager/core/tasks/MediaEntityActorImageFetcherTask.java
+++ src/main/java/org/tinymediamanager/core/tasks/MediaEntityActorImageFetcherTask.java
@@ -15,21 +15,28 @@
  */
 package org.tinymediamanager.core.tasks;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Set;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.tinymediamanager.core.Constants;
 import org.tinymediamanager.core.ImageCache;
 import org.tinymediamanager.core.Utils;
 import org.tinymediamanager.core.entities.MediaEntity;
 import org.tinymediamanager.core.entities.Person;
+import org.tinymediamanager.scraper.http.Url;
 import org.tinymediamanager.scraper.util.UrlUtil;
 
 /**
@@ -98,16 +105,81 @@ public abstract class MediaEntityActorImageFetcherTask implements Runnable {
           return;
         }
 
-        String actorImageFilename = actor.getNameForStorage();
-        if (StringUtils.isBlank(actorImageFilename)) {
-          continue;
-        }
-        Path actorImage = Paths.get(mediaEntity.getPath(), Person.ACTOR_DIR, actorImageFilename);
+        Path tempFile = null;
+        try {
+          String actorImageFilename = actor.getNameForStorage();
+          if (StringUtils.isBlank(actorImageFilename)) {
+            continue;
+          }
+          Path actorImage = Paths.get(mediaEntity.getPath(), Person.ACTOR_DIR, actorImageFilename);
 
-        if (actorImage != null && StringUtils.isNotEmpty(actor.getThumbUrl()) && !Files.exists(actorImage)) {
-          Path cache = ImageCache.getCachedFile(actor.getThumbUrl());
-          if (cache != null) {
-            Utils.copyFileSafe(cache, actorImage);
+          if (actorImage != null && StringUtils.isNotEmpty(actor.getThumbUrl()) && !Files.exists(actorImage)) {
+            Path cache = ImageCache.getCachedFile(actor.getThumbUrl());
+            if (cache != null) {
+              Utils.copyFileSafe(cache, actorImage);
+            }
+            else {
+              // no cache file found - directly download it
+              try {
+                // create a temp file/folder inside the tmm folder
+                Path tempFolder = Paths.get(Constants.TEMP_FOLDER);
+                if (!Files.exists(tempFolder)) {
+                  Files.createDirectory(tempFolder);
+                }
+                tempFile = tempFolder.resolve(actorImageFilename + ".part");
+              }
+              catch (Exception e) {
+                // could not create the temp folder somehow - put the files into the actor folder
+                tempFile = Paths.get(mediaEntity.getPath(), Person.ACTOR_DIR, actorImageFilename + ".part");
+              }
+              Url url = new Url(actor.getThumbUrl());
+              FileOutputStream outputStream = new FileOutputStream(tempFile.toFile());
+              // fetch the images with at max 5 retries
+              InputStream is = url.getInputStreamWithRetry(5);
+
+              if (is == null) {
+                // 404 et all
+                IOUtils.closeQuietly(outputStream);
+                throw new FileNotFoundException("Error accessing url: " + url.getStatusLine());
+              }
+              IOUtils.copy(is, outputStream);
+              outputStream.flush();
+              try {
+                outputStream.getFD().sync(); // wait until file has been completely written
+                // give it a few milliseconds
+                Thread.sleep(150);
+              }
+              catch (Exception ignored) {
+                // empty here -> just not let the thread crash
+              }
+              IOUtils.closeQuietly(outputStream);
+              IOUtils.closeQuietly(is);
+
+              // check if the file has been downloaded
+              if (!Files.exists(tempFile) || Files.size(tempFile) == 0) {
+                // cleanup the file
+                FileUtils.deleteQuietly(tempFile.toFile());
+                throw new Exception("0byte file downloaded: " + actorImageFilename);
+              }
+
+              // delete new destination if existing
+              Utils.deleteFileSafely(actorImage);
+
+              // move the temp file to the expected filename
+              if (!Utils.moveFileSafe(tempFile, actorImage)) {
+                throw new Exception("renaming temp file failed: " + actorImageFilename);
+              }
+            }
+          }
+        }
+        catch (Exception e) {
+          LOGGER.warn("Problem downloading actor artwork: " + e.getMessage());
+        }
+        finally {
+          // remove temp file
+          // Path tempFile = entity.getPathNIO().resolve(filename + "." + timestamp + ".part"); // multi episode same file
+          if (tempFile != null && Files.exists(tempFile)) {
+            Utils.deleteFileSafely(tempFile);
           }
         }
         // else {
