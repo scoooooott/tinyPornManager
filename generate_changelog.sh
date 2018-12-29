517affe3c252c9fabecbf2f69baf093057795387
Manuel Laggner <manuel.laggner@gmail.com>
2018-12-29 11:53:12 +0100

    fix: changed generate_changelog.sh - show commits from the last 2 months
    [ci skip]

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 rm changelog.txt
 DATE=$(date +%F)
-git log --no-merges --format="%cd" --date=short --invert-grep --grep="Translated using Weblate" --since="2016-01-20 00:00:00" refs/remotes/origin/devel.. | sort -u -r | while read DATE ; do
+START_DATE==$(date --date="-2 month" +'%Y-%m-%d 00:00:00')
+git log --no-merges --format="%cd" --date=short --invert-grep --grep="Translated using Weblate" --since="$START_DATE" | sort -u -r | while read DATE ; do
   echo >> changelog.txt
   echo [$DATE] >> changelog.txt
-  GIT_PAGER=cat git log --no-merges --format=" * %s" --since="$DATE 00:00:00" --until="$DATE 23:59:59" --invert-grep --grep="Translated using Weblate" refs/remotes/origin/devel.. >> changelog.txt
+  GIT_PAGER=cat git log --no-merges --format=" * %s" --since="$DATE 00:00:00" --until="$DATE 23:59:59" --invert-grep --grep="Translated using Weblate" >> changelog.txt
 done
