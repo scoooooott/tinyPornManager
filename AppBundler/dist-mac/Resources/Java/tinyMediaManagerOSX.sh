d86fcd27bff483fdbe109c7966c473676151fe37
Manuel Laggner <manuel.laggner@gmail.com>
2018-12-31 16:32:00 +0100

    fix: java 8+ check for OSX

--- AppBundler/dist-mac/Resources/Java/tinyMediaManagerOSX.sh
+++ AppBundler/dist-mac/Resources/Java/tinyMediaManagerOSX.sh
@@ -14,21 +14,21 @@
 # find the path where to execute tmm
 PRG=$0
 while [ -h "$PRG" ]; do
-    ls=`ls -ld "$PRG"`
-    link=`expr "$ls" : '^.*-> \(.*\)$' 2>/dev/null`
-    if expr "$link" : '^/' 2> /dev/null >/dev/null; then
-        PRG="$link"
-    else
-        PRG="`dirname "$PRG"`/$link"
-    fi
+	ls=$(ls -ld "$PRG")
+	link=$(expr "$ls" : '^.*-> \(.*\)$' 2>/dev/null)
+	if expr "$link" : '^/' 2>/dev/null >/dev/null; then
+		PRG="$link"
+	else
+		PRG="$(dirname "$PRG")/$link"
+	fi
 done
 
-progdir=`dirname "$PRG"`
+progdir=$(dirname "$PRG")
 
 # check if tmm has been executed in a read only environment
 if [ ! -w "$progdir" ]; then
-  osascript -e "tell application \"System Events\" to display dialog \"ERROR launching tinyMediaManager!\n\nYou need to execute tinyMediaManager from a writeable location (e.g. the Applications folder)\" with title \"tinyMediaManager\" buttons {\" OK \"} default button 1 with icon path to resource \"tmm.icns\" in bundle (path to me)"
-  exit 1
+	osascript -e "tell application \"System Events\" to display dialog \"ERROR launching tinyMediaManager!\n\nYou need to execute tinyMediaManager from a writeable location (e.g. the Applications folder)\" with title \"tinyMediaManager\" buttons {\" OK \"} default button 1 with icon path to resource \"tmm.icns\" in bundle (path to me)"
+	exit 1
 fi
 
 # By default Mac OS X LC_ALL is set to "C", which means files with special characters will not be found.
@@ -36,36 +36,37 @@ export LC_ALL="en_US.UTF-8"
 
 # search for the right JVM - priority is java 8
 if [ -x /usr/libexec/java_home ]; then
-  JAVA_HOME="`/usr/libexec/java_home -v 1.8 -F`"
-  export JAVA_HOME
+	JAVA_HOME="$(/usr/libexec/java_home -v 1.8+ -F)"
+	export JAVA_HOME
 fi
 
-if [ ! -f "$JAVA_HOME/bin/java" -a -x "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin/java" ]; then
-  JAVA_HOME="/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home"
-  export JAVA_HOME
+if [ ! -f "$JAVA_HOME/bin/java" ] && [ -x "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin/java" ]; then
+	JAVA_HOME="/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home"
+	export JAVA_HOME
 fi
 JAVACMD="${JAVA_HOME}/bin/java"
 
-if [ ! -f "$JAVACMD" -o ! -x "$JAVACMD" ]; then
-  # display error message with applescript
-  osascript -e "tell application \"System Events\" to display dialog \"ERROR launching tinyMediaManager!\n\nYou need to have JAVA installed on your Mac!\nVisit http://java.com for more information...\" with title \"tinyMediaManager\" buttons {\" OK \"} default button 1 with icon path to resource \"tmm.icns\" in bundle (path to me)"
-  
-  # and open java.com
-  open http://java.com
-  
-  # exit with error
-  exit 1
+if [ ! -f "$JAVACMD" ] || [ ! -x "$JAVACMD" ]; then
+	# display error message with applescript
+	osascript -e "tell application \"System Events\" to display dialog \"ERROR launching tinyMediaManager!\n\nYou need to have JAVA installed on your Mac!\nVisit http://java.com for more information...\" with title \"tinyMediaManager\" buttons {\" OK \"} default button 1 with icon path to resource \"tmm.icns\" in bundle (path to me)"
+
+	# and open java.com
+	open http://java.com
+
+	# exit with error
+	exit 1
 fi
 
 # have a look if we need to launch the updater or tmm directly
 if [ -f tmm.jar ]; then
-  ARGS="-Dsilent=noupdate"
+	ARGS="-Dsilent=noupdate"
 else
-  ARGS="-Xdock:name=`tinyMediaManager updater`"
-  ARGS="$ARGS -Xdock:icon=../tmm.icns"
+	ARGS="-Xdock:name=$(tinyMediaManager updater)"
+	ARGS="$ARGS -Xdock:icon=../tmm.icns"
 fi
 
 ARGS="$ARGS -Djava.net.preferIPv4Stack=true -Dappbase=http://www.tinymediamanager.org/"
 
 # execute it :)
-exec "$JAVACMD" ${ARGS} -jar getdown.jar .      
+# shellcheck disable=SC2086
+exec "$JAVACMD" ${ARGS} -jar getdown.jar .
