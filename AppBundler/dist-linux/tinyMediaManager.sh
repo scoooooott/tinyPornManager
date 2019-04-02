7c3e54120f17b459ae9358c7736f316b585f2aea
Peter J. Mello <admin@petermello.net>
2019-04-02 02:00:39 +0000

    Correct URI protocol to https from http

--- AppBundler/dist-linux/tinyMediaManager.sh
+++ AppBundler/dist-linux/tinyMediaManager.sh
@@ -2,23 +2,24 @@
 #
 # tinyMediaManager v3 by Manuel Laggner
 # https://www.tinymediamanager.org/
-# SPDX­License­Identifier: Apache-2.0
+# SPDX-License-Identifier: Apache-2.0
 #
 # Launch tmm (or the updater if tmm.jar is missing)
 
+# Allow the script to be called from any directory and through symlinks
+TMM_DIR="$(dirname "$(test -L "${BASH_SOURCE[0]}" && \
+    readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")"
+
 # Cancel the updater if tmm.jar is present for execution
 if [ -f "$TMM_DIR/tmm.jar" ]; then
-  ARGS="-Dsilent=noupdate"
+    PARAMS="-Dsilent=noupdate"
 fi
 
 # Use IPv4 when possible and declare the appbase
-ARGS="$ARGS -Djava.net.preferIPv4Stack=true \
-  -Dappbase=http://www.tinymediamanager.org/"
-
-# Allow the script to be called from any directory and through symlinks
-TMM_DIR="$(dirname "$(test -L "${BASH_SOURCE[0]}" && \
-  readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")"
+PARAMS+=("-Djava.net.preferIPv4Stack=true"
+        "-Dappbase=https://www.tinymediamanager.org/" "-jar"
+)
 
 # Ma! Start the car! :)
 cd "$TMM_DIR" || return 1
-java "$ARGS" -jar getdown.jar .
+java "${PARAMS[@]}" getdown.jar .
