aca3bfb609df2641908ad809eb049174d1bac627
Manuel Laggner <manuel.laggner@gmail.com>
2019-09-26 15:17:11 +0200

    feature: added an action to force deletion of all HTTP caches
    fixes #656

---
+++ src/main/java/org/tinymediamanager/ui/actions/ClearHttpCacheAction.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2012 - 2019 Manuel Laggner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tinymediamanager.ui.actions;
+
+import java.awt.event.ActionEvent;
+import java.util.ResourceBundle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tinymediamanager.scraper.http.InMemoryCachedUrl;
+import org.tinymediamanager.scraper.http.TmmHttpClient;
+import org.tinymediamanager.ui.UTF8Control;
+
+/**
+ * The ClearHttpCacheAction is used to completely clear the HTTP cache
+ * 
+ * @author Manuel Laggner
+ */
+public class ClearHttpCacheAction extends TmmAction {
+  private static final long           serialVersionUID = -4615019451671427233L;
+  private static final ResourceBundle BUNDLE           = ResourceBundle.getBundle("messages", new UTF8Control()); //$NON-NLS-1$
+  private final static Logger         LOGGER           = LoggerFactory.getLogger(ClearHttpCacheAction.class);
+
+  public ClearHttpCacheAction() {
+    putValue(NAME, BUNDLE.getString("tmm.clearhttpcache")); //$NON-NLS-1$
+    putValue(SHORT_DESCRIPTION, BUNDLE.getString("tmm.clearhttpcache")); //$NON-NLS-1$
+  }
+
+  @Override
+  protected void processAction(ActionEvent arg0) {
+    try {
+      TmmHttpClient.clearCache();
+      InMemoryCachedUrl.clearCache();
+    }
+    catch (Exception e) {
+      LOGGER.warn("could not delete HTTP cache: {}", e.getMessage());
+    }
+  }
+}
