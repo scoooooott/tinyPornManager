06e1649ac1ac286f2c5ee89d5b1aa509b3c8f6dd
Manuel Laggner <manuel.laggner@gmail.com>
2019-10-04 11:22:15 +0200

    fix: fixed TV show cleanup action title
    [ci skip]

--- src/main/java/org/tinymediamanager/ui/tvshows/actions/TvShowCleanUpFilesAction.java
+++ src/main/java/org/tinymediamanager/ui/tvshows/actions/TvShowCleanUpFilesAction.java
@@ -1,5 +1,12 @@
 package org.tinymediamanager.ui.tvshows.actions;
 
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javax.swing.JOptionPane;
+
 import org.tinymediamanager.core.entities.MediaEntity;
 import org.tinymediamanager.ui.IconManager;
 import org.tinymediamanager.ui.MainWindow;
@@ -8,20 +15,14 @@ import org.tinymediamanager.ui.actions.TmmAction;
 import org.tinymediamanager.ui.dialogs.CleanUpUnwantedFilesDialog;
 import org.tinymediamanager.ui.tvshows.TvShowUIModule;
 
-import javax.swing.JOptionPane;
-import java.awt.event.ActionEvent;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
-
 public class TvShowCleanUpFilesAction extends TmmAction {
 
   private static final ResourceBundle BUNDLE = ResourceBundle.getBundle("messages", new UTF8Control()); //$NON-NLS-1$
 
   public TvShowCleanUpFilesAction() {
 
-    putValue(NAME, BUNDLE.getString("tvshow.cleanupfiles")); //$NON-NLS-1$
-    putValue(SHORT_DESCRIPTION, BUNDLE.getString("movie.cleanupfiles.desc")); //$NON-NLS-1$
+    putValue(NAME, BUNDLE.getString("cleanupfiles")); //$NON-NLS-1$
+    putValue(SHORT_DESCRIPTION, BUNDLE.getString("cleanupfiles.desc")); //$NON-NLS-1$
     putValue(SMALL_ICON, IconManager.DELETE);
     putValue(LARGE_ICON_KEY, IconManager.DELETE);
 
