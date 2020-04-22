85d0397fb78c104e4752e9b6dfe5c5570bd87339
Wolfgang Janes <wolfgang.janes@kitz.net>
2020-04-22 20:24:46 +0200

    addod support for xyplorer

--- src/main/java/org/tinymediamanager/ui/TmmUIHelper.java
+++ src/main/java/org/tinymediamanager/ui/TmmUIHelper.java
@@ -312,14 +312,23 @@ public class TmmUIHelper {
       }
     }
     else if (SystemUtils.IS_OS_WINDOWS) {
-      // try to open directly
+      boolean started = false;
+      // try xyplorer first
       try {
-        Desktop.getDesktop().open(file.toFile());
+        exec(new String[]{"xyplorer", abs});
+        started = true;
+      } catch (Exception ignored) {
+        // nothing to do here
       }
-      catch (Exception e) {
-        // use explorer directly - ship around access exceptions and the unresolved network bug
-        // http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6780505
-        exec(new String[] { "explorer", abs });
+      // try to open directly
+      if (!started) {
+        try {
+          Desktop.getDesktop().open(file.toFile());
+        } catch (Exception e) {
+          // use explorer directly - ship around access exceptions and the unresolved network bug
+          // http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6780505
+          exec(new String[]{"explorer", abs});
+        }
       }
     }
     else if (SystemUtils.IS_OS_LINUX) {
